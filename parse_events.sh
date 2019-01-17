6041cd084a0262693b51d07536cbfb006d7cdc01
vinthedark <vinay@singularitynet.io>
2019-01-17 15:42:54 +0530

    Added README.md and health_check file. Needs integration of Health Check with ECS

--- parse_events.sh
+++ parse_events.sh
@@ -5,5 +5,6 @@
 cd /opt/snet/snet-contract-event-consumer/
 nohup node contractevents.js 42 &
 cd -
+export PYTHONPATH=/opt/snet/snet-marketplace-service
 python3 parse_events/handle_contracts.py
 sleep infinity
