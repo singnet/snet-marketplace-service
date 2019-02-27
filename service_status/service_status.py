698ecd0d4fa6d63d162dee3fa21ccc36892c03ec
prashantramangupta <you@example.com>
2019-02-27 10:36:03 +0530

    added to check service status via grpc call

--- service_status/service_status.py
+++ service_status/service_status.py
@@ -1,29 +1,49 @@
 import re
 from datetime import datetime
 from common.utils import Utils
+from service_status.state_service_pb2_grpc import PaymentChannelStateServiceStub
+from service_status.state_service_pb2 import ChannelStateRequest
+import grpc
 
-import requests
 
 class ServiceStatus:
-    def __init__(self, repo):
+    def __init__(self, repo, net_id):
         self.repo = repo
         self.route = '/encoding'
         self.rex_for_pb_ip = '^(http://)*(https://)*127.0.0.1|^(http://)*(https://)*localhost|^(http://)*(https://)*192.|^(http://)*(https://)*172.|^(http://)*(https://)*10.'
         self.obj_util = Utils()
+        self.net_id = net_id
+
+    def _make_grpc_call(self, url, secure=1):
+        channel = None
+        try:
+            if secure==1:
+                channel = grpc.secure_channel(url, grpc.ssl_channel_credentials())
+            else:
+                channel = grpc.insecure_channel(url)
+
+            stub = PaymentChannelStateServiceStub(channel)
+            try:
+                response = stub.GetChannelState(ChannelStateRequest())
+            except grpc.RpcError as err:
+                err_code = str(err.code())
+                print(err_code)
+                if err_code == "StatusCode.UNAVAILABLE":
+                    return 0
+                return 1
+            return 1
+        except Exception as e:
+            print("error in making grpc call::url: ", url, "|err: ", e)
+            return 0
 
     def ping_url(self, url):
         srch_count = re.subn(self.rex_for_pb_ip, '', url)[1]
         if srch_count == 0:
-            if url[:4].lower() != 'http':
-                url = 'http://' + url
-            url = url + self.route
-            try:
-                res = requests.get(url, timeout=5)
-                # res = requests.get(url, timeout=5, verify=False)
-                if res.status_code == 200:
-                    return 1
-            except Exception as err:
-                print("Error {}".format(err))
+            secure = 1
+            if url[:4].lower() == 'http' and url[:5].lower() != 'https':
+                secure = 0
+            url = self.obj_util.remove_http_https_prefix(url=url)
+            return self._make_grpc_call(url=url, secure=secure)
         return 0
 
     def update_service_status(self):
