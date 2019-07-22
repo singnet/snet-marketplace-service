fa5ff6100fef52fa4b1259dddbd0078aa475741c
prashantramangupta <you@example.com>
2019-07-22 20:13:05 +0530

    addition of API's to manage user profile and pre-seeded wallet

---
+++ sign_up/lambda_handler.py
@@ -0,0 +1,78 @@
+import json
+import re
+import traceback
+from common.constant import NETWORKS
+from common.repository import Repository
+from common.utils import Utils
+
+from sign_up.user import User
+
+NETWORKS_NAME = dict((NETWORKS[netId]['name'], netId) for netId in NETWORKS.keys())
+db = dict((netId, Repository(net_id=netId)) for netId in NETWORKS.keys())
+obj_util = Utils()
+
+def request_handler(event, context):
+    print(event)
+    if 'path' not in event:
+        return get_response(400, "Bad Request")
+    try:
+        payload_dict = None
+        path = event['path'].lower()
+        stage = event['requestContext']['stage']
+        net_id = NETWORKS_NAME[stage]
+        usr_obj = User(obj_repo=db[net_id])
+        resp_dta = None
+
+        if event['httpMethod'] == 'POST':
+            payload_dict = json.loads(event['body'])
+        elif event['httpMethod'] == 'GET':
+            payload_dict = event.get('queryStringParameters')
+        else:
+            return get_response(405, "Method Not Allowed")
+
+        if "/signup" == path:
+            resp_dta = usr_obj.user_signup(event['requestContext'])
+
+        elif "/profile" == path and event['httpMethod'] == 'POST':
+            resp_dta = usr_obj.update_user_profile(username=payload_dict['username'], email_alerts=payload_dict['email_alerts'])
+
+        elif "/profile" == path and event['httpMethod'] == 'GET':
+            resp_dta = usr_obj.get_user_profile(username=payload_dict['username'])
+
+        elif "/wallet" == path:
+            status = usr_obj.check_for_existing_wallet(payload_dict['username'])
+            resp_dta = {"isAssigned": status, "username": payload_dict['username']}
+
+        elif re.match("(\/delete-user\/)[^\/]*$", path):
+            username = path.split("/")[2]
+            resp_dta = usr_obj.del_user_data(username=username)
+
+        else:
+            return get_response(404, "Not Found")
+
+        if resp_dta is None:
+            err_msg = {'status': 'failed', 'error': 'Bad Request', 'api': event['path'], 'payload': payload_dict, 'network_id': net_id}
+            obj_util.report_slack(1, str(err_msg))
+            response = get_response(500, err_msg)
+        else:
+            response = get_response(200, {"status": "success", "data": resp_dta})
+    except Exception as e:
+        err_msg = {"status": "failed", "error": repr(e), 'api': event['path'], 'payload': payload_dict, 'network_id': net_id}
+        obj_util.report_slack(1, str(err_msg))
+        response = get_response(500, err_msg)
+        traceback.print_exc()
+    return response
+
+
+def get_response(status_code, message):
+    return {
+        'statusCode': status_code,
+        'body': json.dumps(message),
+        'headers': {
+            'Content-Type': 'application/json',
+            "X-Requested-With": '*',
+            "Access-Control-Allow-Headers": 'Access-Control-Allow-Origin, Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with',
+            "Access-Control-Allow-Origin": '*',
+            "Access-Control-Allow-Methods": 'GET,OPTIONS,POST'
+        }
+    }
---
+++ sign_up/user.py
@@ -0,0 +1,120 @@
+from datetime import datetime as dt
+from common.utils import Utils
+
+class User:
+    def __init__(self, obj_repo):
+        self.repo = obj_repo
+        self.obj_utils = Utils()
+
+    def set_user_info(self, usr_dta):
+        """ Method to set user information. """
+        try:
+            claims = usr_dta['authorizer']['claims']
+            email_verified = claims['email_verified']
+            status = 0
+            if email_verified:
+                status = 1
+            else:
+                raise Exception("Email verification is pending.")
+            q_dta = [claims['cognito:username'], usr_dta['accountId'], claims['name'], claims['email'], status,
+                     status, usr_dta['requestId'], usr_dta['requestTimeEpoch'], dt.utcnow(), dt.utcnow()]
+            set_usr_dta = self.repo.execute(
+                "INSERT INTO user (username, account_id, name, email, email_verified, status, request_id, "
+                "request_time_epoch, row_created, row_updated) " \
+                "VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", q_dta)
+            if len(set_usr_dta) > 0:
+                return "success"
+            else:
+                return "failed"
+        except Exception as e:
+            print(repr(e))
+            raise e
+
+    def check_for_existing_wallet(self, username):
+        """ Method to check for existing wallet address. """
+        try:
+            srch_dta = self.repo.execute("SELECT * FROM wallet WHERE username = %s", username)
+            search_data = self.repo.execute("SELECT count(*) FROM wallet WHERE username = %s", username)
+            if srch_dta == []:
+                print('Username not found');
+                return False
+            return True
+        except Exception as e:
+            print(repr(e))
+            raise e
+
+    def update_wallet(self, username):
+        """ Method to assign wallet address to a user. """
+        try:
+            return self.repo.execute("UPDATE wallet SET username = %s WHERE username is NULL LIMIT 1", username)
+        except Exception as e:
+            print(repr(e))
+            raise e
+
+    def user_signup(self, usr_dta):
+        """ Method to assign pre-seeded wallet to user.
+            This is one time process.
+        """
+        try:
+            self.repo.begin_transaction()
+            username = usr_dta['authorizer']['claims']['cognito:username']
+            set_usr_dta = self.set_user_info(usr_dta)
+            if set_usr_dta == "success":
+                print(set_usr_dta)
+                address_exist = self.check_for_existing_wallet(username=username)
+                if address_exist:
+                    raise Exception('Useraname is already linked to wallet')
+                else:
+                    updt_resp = self.update_wallet(username=username)
+
+                    if updt_resp[0] == 1:
+                        result = self.repo.execute("SELECT * FROM wallet where username = %s", username)
+                        self.repo.execute("UPDATE wallet SET private_key = NULL WHERE username = %s", [username])
+                        self.obj_utils.clean(result)
+                        self.repo.commit_transaction()
+                        return {"success": "success", "data": result}
+                    raise Exception("Error in assigning pre-seeded wallet")
+            elif set_usr_dta == "failed":
+                return "User already exist"
+        except Exception as e:
+            self.repo.rollback_transaction()
+            print(repr(e))
+            raise e
+
+    def del_user_data(self, username):
+        """ Method to delete user data and wallet address.
+            Deregister User.
+        """
+        try:
+            self.repo.begin_transaction()
+            del_user = self.repo.execute("DELETE FROM user WHERE username = %s ", [username])
+            updt_wallet = self.repo.execute("UPDATE wallet SET status=0, username=NULL WHERE username = %s ", [username])
+            self.repo.commit_transaction()
+            return []
+        except Exception as e:
+            self.repo.rollback_transaction()
+            print(repr(e))
+            raise e
+
+    def get_user_profile(self, username):
+        '''
+            Method to fetch user profile data.
+        '''
+        try:
+            result = self.repo.execute("SELECT * FROM user WHERE username = %s", [username])
+            self.obj_utils.clean(result)
+            return {"success": "success", "data": result}
+        except Exception as e:
+            print(repr(e))
+            raise e
+
+    def update_user_profile(self, email_alerts, username):
+        '''
+            Method to update user profile data.
+        '''
+        try:
+            result = self.repo.execute("UPDATE user SET email_alerts = %s WHERE username = %s", [int(email_alerts==True), username])
+            return {"success": "success", "data": []}
+        except Exception as e:
+            print(repr(e))
+            raise e
