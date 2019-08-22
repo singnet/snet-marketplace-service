dc35c17111539e5392a681aa7eae0410ab33e039
Restyled.io <commits@restyled.io>
2019-08-22 13:55:48 +0000

    Restyled by autopep8

--- parse_events/handle_contracts_db.py
+++ parse_events/handle_contracts_db.py
@@ -133,7 +133,8 @@ class HandleContractsDB:
             del_srvc_endpts, [service_id, org_id])
 
         self._del_tags(org_id=org_id, service_id=service_id, conn=conn)
-        print('_del_srvc_dpndts::del_srvc_grps: ', del_srvc_grps_count, '|del_srvc_endpts: ', del_srvc_endpts_count)
+        print('_del_srvc_dpndts::del_srvc_grps: ', del_srvc_grps_count,
+              '|del_srvc_endpts: ', del_srvc_endpts_count)
 
     def _create_or_updt_srvc(self, org_id, service_id, ipfs_hash, conn):
         upsrt_srvc = "INSERT INTO service (org_id, service_id, is_curated, ipfs_hash, row_created, row_updated) " \
