0cf2084c093ebb5be65317ca79e672be85d7163d
prashantramangupta <you@example.com>
2019-08-27 19:08:22 +0530

    config and constants are separated and both these files are at service('parse_events') level

---
+++ parse_events/config.py
@@ -0,0 +1,12 @@
+NETWORK_ID = 0
+NET_ID = 0
+IPFS_URL = {
+    'url': '',
+    'port': '',
+    'protocol': ''
+}
+NETWORKS = {}
+S3_BUCKET_ACCESS_KEY = ""
+S3_BUCKET_SECRET_KEY = ""
+ASSETS_BUCKET_NAME = ""
+ASSETS_PREFIX = ""
