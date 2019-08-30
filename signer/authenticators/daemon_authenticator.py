08b39c9acadda97e31fcf40ffa8d26f3391b1693
Sohit <sumitk002@gmail.com>
2019-08-30 12:11:20 +0530

    fixing type cast issue

--- signer/authenticators/daemon_authenticator.py
+++ signer/authenticators/daemon_authenticator.py
@@ -28,7 +28,7 @@ class SignatureAuthenticator(object):
         pass
 
     def verify_current_block_number(self):
-        signed_block_number = self.event['headers']['x-currentblocknumber']
+        signed_block_number = int(self.event['headers']['x-currentblocknumber'])
         blockchain_util = BlockChainUtil(self.networks[self.net_id]['ws_provider'])
         current_block_number = blockchain_util.get_current_block_no()
         if current_block_number > signed_block_number + self.BLOCK_LIMIT or current_block_number < signed_block_number - self.BLOCK_LIMIT:
