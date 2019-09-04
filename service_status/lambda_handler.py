8687753c6749a60ea23d0ea7b1eb6dfabc4f4c5c
prashantramangupta <you@example.com>
2019-09-04 18:16:42 +0530

    addition of serverless for service status

--- service_status/lambda_handler.py
+++ service_status/lambda_handler.py
@@ -2,7 +2,7 @@ import json
 import re
 import traceback
 
-from service_status.config import NETWORKS
+from service_status.config import NETWORKS, NETWORK_ID
 from common.repository import Repository
 from common.utils import Utils
 from service_status.service_status import ServiceStatus
@@ -16,16 +16,10 @@ db = dict((netId, Repository(net_id=netId, NETWORKS=NETWORKS)) for netId in NETW
 def request_handler(event, context):
     print(event)
     try:
-        stage = event['requestContext']['stage']
-        net_id = NETWORKS_NAME[stage]
-        if db[net_id].connection is None:
-            raise Exception('database connection is not initialized')
-
-        print('update service status', net_id)
-        obj_srvc_st = ServiceStatus(repo=db[net_id], net_id=net_id)
+        obj_srvc_st = ServiceStatus(repo=db[NETWORK_ID], net_id=NETWORK_ID)
         obj_srvc_st.update_service_status()
     except Exception as e:
         print(repr(e))
-        obj_util.report_slack(1, "Error in updating service status::err:" + e)
+        obj_util.report_slack(1, "Error in updating service status::NETWORK_ID:" + NETWORK_ID + "|err:" + e)
         traceback.print_exc()
     return
