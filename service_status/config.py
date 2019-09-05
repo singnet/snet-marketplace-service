804f57bf5065fe7d3f8a8489200d3691cc0196da
prashantramangupta <you@example.com>
2019-09-05 13:17:16 +0530

    addition of serverless in service status service

--- service_status/config.py
+++ service_status/config.py
@@ -1,4 +1,16 @@
-NETWORKS = {}
+NETWORKS = {
+    0: {
+        'name': 'TEST',
+        'http_provider': 'https://ropsten.infura.io',
+        'ws_provider': 'wss://ropsten.infura.io/ws',
+        'db': {'DB_HOST': '127.0.0.1',
+               'DB_USER': 'unittest_root',
+               'DB_PASSWORD': 'unittest_pwd',
+               'DB_NAME': 'unittest_db',
+               'DB_PORT': 3306
+               }
+    } 
+}
 SLACK_HOOK = {
     'hostname' : '',
     'port': 443,
@@ -8,3 +20,4 @@ SLACK_HOOK = {
         'Content-Type': 'application/json'
     }
 }
+NETWORK_ID = 0
