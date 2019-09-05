1a0fd8d631885d03bc022df1b57d3ac9fc4501ce
Restyled.io <commits@restyled.io>
2019-09-05 07:09:51 +0000

    Restyled by autopep8

--- service_status/lambda_handler.py
+++ service_status/lambda_handler.py
@@ -10,7 +10,8 @@ from service_status.service_status import ServiceStatus
 NETWORKS_NAME = dict((NETWORKS[netId]['name'], netId)
                      for netId in NETWORKS.keys())
 obj_util = Utils()
-db = dict((netId, Repository(net_id=netId, NETWORKS=NETWORKS)) for netId in NETWORKS.keys())
+db = dict((netId, Repository(net_id=netId, NETWORKS=NETWORKS))
+          for netId in NETWORKS.keys())
 
 
 def request_handler(event, context):
@@ -20,6 +21,7 @@ def request_handler(event, context):
         obj_srvc_st.update_service_status()
     except Exception as e:
         print(repr(e))
-        obj_util.report_slack(1, "Error in updating service status::NETWORK_ID:" + NETWORK_ID + "|err:" + e)
+        obj_util.report_slack(
+            1, "Error in updating service status::NETWORK_ID:" + NETWORK_ID + "|err:" + e)
         traceback.print_exc()
     return
