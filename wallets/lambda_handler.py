9970ced79241d7867bbc55cfaad18f3178405b50
Restyled.io <commits@restyled.io>
2019-09-16 12:16:55 +0000

    Restyled by yapf

--- wallets/lambda_handler.py
+++ wallets/lambda_handler.py
@@ -11,7 +11,8 @@ from wallets.config import NETWORKS
 from wallets.constant import REQUIRED_KEYS_FOR_LAMBDA_EVENT
 from wallets.wallet_service import WalletService
 
-NETWORKS_NAME = dict((NETWORKS[netId]["name"], netId) for netId in NETWORKS.keys())
+NETWORKS_NAME = dict(
+    (NETWORKS[netId]["name"], netId) for netId in NETWORKS.keys())
 db = dict((netId, Repository(net_id=netId)) for netId in NETWORKS.keys())
 obj_util = Utils()
 
@@ -42,8 +43,7 @@ def route_path(path, method, payload_dict):
 
     elif "/wallet/status" == path:
         response_data = obj_wallet_manager.update_wallet_status(
-            address=payload_dict["address"]
-        )
+            address=payload_dict["address"])
     else:
         path_exist = False
 
@@ -53,21 +53,21 @@ def route_path(path, method, payload_dict):
 def request_handler(event, context):
     try:
         valid_event = validate_dict(
-            event=event, required_keys=REQUIRED_KEYS_FOR_LAMBDA_EVENT
-        )
+            event=event, required_keys=REQUIRED_KEYS_FOR_LAMBDA_EVENT)
         if not valid_event:
             return generate_lambda_response(400, "Bad Request")
 
         path = event["path"].lower()
         method = event["httpMethod"]
 
-        method_found, payload_dict = extract_payload(method=method, event=event)
+        method_found, payload_dict = extract_payload(method=method,
+                                                     event=event)
         if not method_found:
             return generate_lambda_response(405, "Method Not Allowed")
 
-        path_exist, response_data = route_path(
-            path=path, method=method, payload_dict=payload_dict
-        )
+        path_exist, response_data = route_path(path=path,
+                                               method=method,
+                                               payload_dict=payload_dict)
         if not path_exist:
             return generate_lambda_response(404, "Not Found")
 
@@ -82,9 +82,10 @@ def request_handler(event, context):
             obj_util.report_slack(1, error_message)
             response = generate_lambda_response(500, error_message)
         else:
-            response = generate_lambda_response(
-                200, {"status": "success", "data": response_data}
-            )
+            response = generate_lambda_response(200, {
+                "status": "success",
+                "data": response_data
+            })
     except Exception as e:
         error_message = format_error_message(
             status="failed",
