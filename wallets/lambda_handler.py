0356464c2a6e70e41ef3e299ecb09b77d8e3889a
Restyled.io <commits@restyled.io>
2019-09-18 12:12:23 +0000

    Restyled by autopep8

--- wallets/lambda_handler.py
+++ wallets/lambda_handler.py
@@ -6,10 +6,10 @@ from wallets.config import NETWORKS, NETWORK_ID
 from wallets.constant import REQUIRED_KEYS_FOR_LAMBDA_EVENT
 from wallets.wallet_service import WalletService
 
-NETWORKS_NAME = dict((NETWORKS[netId]['name'], netId) for netId in NETWORKS.keys())
+NETWORKS_NAME = dict((NETWORKS[netId]['name'], netId)
+                     for netId in NETWORKS.keys())
 db = dict((netId, Repository(net_id=netId)) for netId in NETWORKS.keys())
 obj_util = Utils()
-
 
 
 def route_path(path, method, payload_dict):
@@ -34,7 +34,8 @@ def route_path(path, method, payload_dict):
                                                                 currency=payload_dict['currency'])
 
     elif "/wallet/status" == path:
-        response_data = obj_wallet_manager.update_wallet_status(address=payload_dict['address'])
+        response_data = obj_wallet_manager.update_wallet_status(
+            address=payload_dict['address'])
     else:
         path_exist = False
 
@@ -43,18 +44,21 @@ def route_path(path, method, payload_dict):
 
 def request_handler(event, context):
     try:
-        valid_event = validate_dict(event=event, required_keys=REQUIRED_KEYS_FOR_LAMBDA_EVENT)
+        valid_event = validate_dict(
+            event=event, required_keys=REQUIRED_KEYS_FOR_LAMBDA_EVENT)
         if not valid_event:
             return generate_lambda_response(400, "Bad Request")
 
         path = event['path'].lower()
         method = event['httpMethod']
 
-        method_found, payload_dict = extract_payload(method=method, event=event)
+        method_found, payload_dict = extract_payload(
+            method=method, event=event)
         if not method_found:
             return generate_lambda_response(405, "Method Not Allowed")
 
-        path_exist, response_data = route_path(path=path, method=method, payload_dict=payload_dict)
+        path_exist, response_data = route_path(
+            path=path, method=method, payload_dict=payload_dict)
         if not path_exist:
             return generate_lambda_response(404, "Not Found")
 
@@ -64,7 +68,8 @@ def request_handler(event, context):
             obj_util.report_slack(1, error_message)
             response = generate_lambda_response(500, error_message)
         else:
-            response = generate_lambda_response(200, {"status": "success", "data": response_data})
+            response = generate_lambda_response(
+                200, {"status": "success", "data": response_data})
     except Exception as e:
         error_message = format_error_message(status="failed", error="Bad Request", resource=path,
                                              payload=payload_dict, net_id=NETWORK_ID)
--- wallets/wallet_service.py
+++ wallets/wallet_service.py
@@ -12,9 +12,11 @@ class WalletService:
         self.obj_repo = obj_repo
 
     def create_and_register_wallet(self):
-        self.obj_blockchain_util = BlockChainUtil(provider_type="HTTP_PROVIDER", provider=NETWORKS[NETWORK_ID]['http_provider'])
+        self.obj_blockchain_util = BlockChainUtil(
+            provider_type="HTTP_PROVIDER", provider=NETWORKS[NETWORK_ID]['http_provider'])
         address, private_key = self.obj_blockchain_util.create_account()
-        obj_wallet = Wallet(address=address, private_key=private_key, type=GENERAL_WALLET_TYPE, status=0)
+        obj_wallet = Wallet(
+            address=address, private_key=private_key, type=GENERAL_WALLET_TYPE, status=0)
         registered = self.register_wallet(obj_wallet=obj_wallet)
         if registered:
             return obj_wallet.get_wallet()
@@ -37,7 +39,8 @@ class WalletService:
                   group_id, agi_tokens, expiration, message_nonce]
         signature = self.obj_blockchain_util.generate_signature(data_types=data_types, values=values,
                                                                 signer_key=signer_key)
-        v, r, s = Web3.toInt(hexstr="0x" + signature[-2:]), signature[:66], "0x" + signature[66:130]
+        v, r, s = Web3.toInt(
+            hexstr="0x" + signature[-2:]), signature[:66], "0x" + signature[66:130]
         return r, s, v, signature
 
     def __calculate_agi_tokens(self, amount, currency):
@@ -51,11 +54,13 @@ class WalletService:
     def open_channel_by_third_party(self, order_id, sender, sender_private_key, group_id, amount, currency, recipient):
         obj_wallet_dao = WalletDAO(obj_repo=self.obj_repo)
         method_name = "openChannelByThirdParty"
-        self.obj_blockchain_util = BlockChainUtil(provider_type="HTTP_PROVIDER", provider=NETWORKS[NETWORK_ID]['http_provider'])
+        self.obj_blockchain_util = BlockChainUtil(
+            provider_type="HTTP_PROVIDER", provider=NETWORKS[NETWORK_ID]['http_provider'])
         current_block_no = self.obj_blockchain_util.get_current_block_no()
         # 1 block no is mined in 15 sec on average, setting expiration as 10 years
         expiration = current_block_no + (10 * 365 * 24 * 60 * 4)
-        agi_tokens = self.__calculate_agi_tokens(amount=amount, currency=currency)
+        agi_tokens = self.__calculate_agi_tokens(
+            amount=amount, currency=currency)
         r, s, v, signature = self.__generate_signature_details(recipient=recipient, group_id=group_id,
                                                                agi_tokens=agi_tokens, expiration=expiration,
                                                                message_nonce=current_block_no,
@@ -70,7 +75,8 @@ class WalletService:
                                                                       net_id=self.net_id)
         raw_transaction = self.obj_utils.sign_transaction_with_private_key(transaction_object=transaction_object,
                                                                            private_key=EXECUTOR_KEY)
-        transaction_hash = self.obj_utils.process_raw_transaction(raw_transaction=raw_transaction)
+        transaction_hash = self.obj_utils.process_raw_transaction(
+            raw_transaction=raw_transaction)
         print("openChannelByThirdParty::transaction_hash", transaction_hash)
         obj_wallet_dao.insert_channel_history(order_id=order_id, amount=amount, currency=currency, type=method_name,
                                               address=sender, signature=signature,
@@ -85,7 +91,8 @@ class WalletService:
 
     def add_funds_to_channel(self, order_id, channel_id, amount, currency):
         method_name = "channelAddFunds"
-        agi_tokens = self.__calculate_agi_tokens(amount=amount, currency=currency)
+        agi_tokens = self.__calculate_agi_tokens(
+            amount=amount, currency=currency)
         positional_inputs = (channel_id, agi_tokens)
         transaction_object = self.obj_utils.create_transaction_object(*positional_inputs, method_name=method_name,
                                                                       address=EXECUTOR_ADDRESS,
@@ -95,7 +102,8 @@ class WalletService:
         raw_transaction = self.obj_utils.sign_transaction_with_private_key(transaction_object=transaction_object,
                                                                            private_key=EXECUTOR_KEY)
 
-        transaction_hash = self.obj_utils.process_raw_transaction(raw_transaction=raw_transaction)
+        transaction_hash = self.obj_utils.process_raw_transaction(
+            raw_transaction=raw_transaction)
         print("channelAddFunds::transaction_hash", transaction_hash)
         return {"transaction_hash": transaction_hash, "agi_tokens": agi_tokens,
                 "positional_inputs": positional_inputs, "type": method_name}
