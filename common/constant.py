223fe519e574196591a52cb7378be6f1b824ee3d
Pratik Prajapati <pratikap98@gmail.com>
2019-09-18 17:46:21 +0530

    change after PR review

--- common/constant.py
+++ common/constant.py
@@ -3,9 +3,6 @@ REG_CNTRCT_PATH = COMMON_CNTRCT_PATH + '/abi/Registry.json'
 MPE_CNTRCT_PATH = COMMON_CNTRCT_PATH + '/abi/MultiPartyEscrow.json'
 REG_ADDR_PATH = COMMON_CNTRCT_PATH + '/networks/Registry.json'
 MPE_ADDR_PATH = COMMON_CNTRCT_PATH + '/networks/MultiPartyEscrow.json'
-STATUS_CODE_SUCCESS = 200
-STATUS_CODE_REDIRECT = 301
-STATUS_CODE_FAILED = 500
 
 
 class StatusCode:
@@ -13,10 +10,3 @@ class StatusCode:
     INTERNAL_SERVER_ERROR = 500
     CREATED = 201
     OK = 200
-
-
-HEADER_POST_RESPONSE = {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "POST"
-}
