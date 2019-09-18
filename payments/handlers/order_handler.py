d592c8c95483c6241b2116a64562d3fbe736d548
Pratik Prajapati <pratikap98@gmail.com>
2019-09-18 17:13:19 +0530

    change after PR review

--- payments/handlers/order_handler.py
+++ payments/handlers/order_handler.py
@@ -1,25 +1,31 @@
 import json
 
 from common.constant import StatusCode
-from common.utils import generate_lambda_response
+from common.utils import generate_lambda_response, validate_dict
 from payments.application.dapp_order_manager import OrderManager
 
 
 def create(event, context):
     payload = event["body"]
     item_details = json.loads(event['body'])
-    amount = payload["amount"]["amount"]
-    currency = payload["amount"]["currency"]
-    username = payload["username"]
+    if validate_dict(payload, ["amount", "username"]):
+        amount = payload["amount"]["amount"]
+        currency = payload["amount"]["currency"]
+        username = payload["username"]
 
-    status, response = OrderManager().create_order(amount, currency, item_details, username)
-    if status:
-        return generate_lambda_response(
-            status_code=StatusCode.SUCCESS_POST_CODE,
-            message=response
-        )
+        status, response = OrderManager().create_order(amount, currency, item_details, username)
+        if status:
+            return generate_lambda_response(
+                status_code=StatusCode.CREATED,
+                message=response
+            )
+        else:
+            return generate_lambda_response(
+                status_code=StatusCode.INTERNAL_SERVER_ERROR,
+                message=response
+            )
     else:
         return generate_lambda_response(
-            status_code=StatusCode.SERVER_ERROR_CODE,
-            message=response
+            status_code=StatusCode.BAD_REQUEST,
+            message="Bad Request"
         )
--- payments/handlers/payment_handler.py
+++ payments/handlers/payment_handler.py
@@ -1,6 +1,6 @@
 import json
 
-from common.utils import make_response, generate_lambda_response
+from common.utils import make_response, generate_lambda_response, validate_dict
 from payments.application.dapp_order_manager import OrderManager
 from common.constant import StatusCode, HEADER_POST_RESPONSE
 
@@ -8,19 +8,25 @@ from common.constant import StatusCode, HEADER_POST_RESPONSE
 def initiate(event, context):
     order_id = event['pathParameters']["order_id"]
     payload = json.loads(event['body'])
-    amount = payload["amount"]["amount"]
-    currency = payload["amount"]["currency"]
-    payment_method = payload["payment_method"]
-    status, response = OrderManager().initiate_payment_against_order(order_id, amount, currency, payment_method)
-    if status:
-        return generate_lambda_response(
-            status_code=StatusCode.SUCCESS_POST_CODE,
-            message=response
-         )
+    if validate_dict(payload, ["amount", "payment_method"]):
+        amount = payload["amount"]["amount"]
+        currency = payload["amount"]["currency"]
+        payment_method = payload["payment_method"]
+        status, response = OrderManager().initiate_payment_against_order(order_id, amount, currency, payment_method)
+        if status:
+            return generate_lambda_response(
+                status_code=StatusCode.CREATED,
+                message=response
+             )
+        else:
+            return generate_lambda_response(
+                status_code=StatusCode.INTERNAL_SERVER_ERROR,
+                message=response
+            )
     else:
         return generate_lambda_response(
-            status_code=StatusCode.SERVER_ERROR_CODE,
-            message=response
+            status_code=StatusCode.BAD_REQUEST,
+            message="Bad Request"
         )
 
 
@@ -28,16 +34,22 @@ def execute(event, context):
     order_id = event['pathParameters']["order_id"]
     payment_id = event['pathParameters']["payment_id"]
     payload = json.loads(event['body'])
-    payment_method = payload["payment_method"]
-    payment_details = payload["payment_details"]
-    status, response = OrderManager().execute_payment_against_order(order_id, payment_id, payment_details, payment_method)
-    if status:
-        return generate_lambda_response(
-            status_code=StatusCode.SUCCESS_POST_CODE,
-            message=response
-        )
+    if validate_dict(payload, ["payment_method", "payment_details"]):
+        payment_method = payload["payment_method"]
+        payment_details = payload["payment_details"]
+        status, response = OrderManager().execute_payment_against_order(order_id, payment_id, payment_details, payment_method)
+        if status:
+            return generate_lambda_response(
+                status_code=StatusCode.CREATED,
+                message=response
+            )
+        else:
+            return generate_lambda_response(
+                status_code=StatusCode.INTERNAL_SERVER_ERROR,
+                message=response
+            )
     else:
         return generate_lambda_response(
-            status_code=StatusCode.SERVER_ERROR_CODE,
-            message=response
+            status_code=StatusCode.BAD_REQUEST,
+            message="Bad Request"
         )
