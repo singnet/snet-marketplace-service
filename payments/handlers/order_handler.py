7bd6bbcb5cf5519ab9b5e996699df6b947494ac3
Pratik Prajapati <pratikap98@gmail.com>
2019-09-19 12:35:33 +0530

    response rectification

--- payments/handlers/order_handler.py
+++ payments/handlers/order_handler.py
@@ -1,30 +1,40 @@
 import json
 
 from common.constant import StatusCode
+from common.logger import get_logger
 from common.utils import generate_lambda_response, validate_dict
 from payments.application.dapp_order_manager import OrderManager
 
+logger = get_logger(__name__)
+
 
 def create(event, context):
-    payload = json.loads(event['body'])
-    if validate_dict(payload, ["price", "username"]):
-        amount = payload["price"]["amount"]
-        currency = payload["price"]["currency"]
-        username = payload["username"]
-        item_details = payload["item_details"]
-        status, response = OrderManager().create_order(amount, currency, item_details, username)
-        if status:
-            return generate_lambda_response(
-                status_code=StatusCode.CREATED,
-                message=response
-            )
+    try:
+        payload = json.loads(event['body'])
+        if validate_dict(payload, ["price", "username"]):
+            amount = payload["price"]["amount"]
+            currency = payload["price"]["currency"]
+            username = payload["username"]
+            item_details = payload["item_details"]
+            status, response = OrderManager().create_order(amount, currency, item_details, username)
+            if status:
+                return generate_lambda_response(
+                    status_code=StatusCode.CREATED,
+                    message=response
+                )
+            else:
+                return generate_lambda_response(
+                    status_code=StatusCode.INTERNAL_SERVER_ERROR,
+                    message=response
+                )
         else:
             return generate_lambda_response(
-                status_code=StatusCode.INTERNAL_SERVER_ERROR,
-                message=response
+                status_code=StatusCode.BAD_REQUEST,
+                message="Bad Request"
             )
-    else:
+    except Exception as e:
+        logger(e)
         return generate_lambda_response(
-            status_code=StatusCode.BAD_REQUEST,
-            message="Bad Request"
+            status_code=StatusCode.INTERNAL_SERVER_ERROR,
+            message="Internal Server Error"
         )
