e743add4c2430d0a8e21c4edc0cb79da1b00c4e9
Pratik Prajapati <pratikap98@gmail.com>
2019-09-19 10:52:53 +0530

    test cases updated and response rectification

--- payments/domain/tests/test_dapp_manager.py
+++ payments/domain/tests/test_dapp_manager.py
@@ -12,17 +12,17 @@ class TestDappMananger(unittest.TestCase):
     dapp_order_manager = OrderManager()
 
     def test_create_order(self):
-        response = self.dapp_order_manager.create_order(100, "USD", {"item": "item", "quantity": 2}, "user")
-        assert "body" in response
-        assert "headers" in response
-        assert "statusCode" in response
-        assert "order_id" in response["body"]
-        assert "item_details" in response["body"]
-        assert "amount" in response["body"]
-        assert "currency" in response["body"]
-        order_id = response["body"]["order_id"]
-        print(response)
-        self.repo.session.query(Order.id == order_id).delete()
+        status, response = self.dapp_order_manager.create_order(100, "USD", {"item": "item", "quantity": 2}, "user")
+        if status:
+            assert "order_id" in response
+            assert "item_details" in response
+            if "amount" in response:
+                assert "amount" in response["amount"]
+                assert "currency" in response["amount"]
+            else:
+                assert False
+            order_id = response["order_id"]
+            self.repo.session.query(Order.id == order_id).delete()
 
     def test_initiate_order(self):
         self.repo.add_item(
@@ -40,27 +40,25 @@ class TestDappMananger(unittest.TestCase):
                 username="user",
             )
         )
-        response = self.dapp_order_manager.initiate_payment_against_order("order_test_123", 100, "USD", "paypal")
-        assert "body" in response
-        assert "headers" in response
-        assert "statusCode" in response
-        assert "amount" in response["body"]
-        assert "currency" in response["body"]
-        assert "payment_id" in response["body"]
-        assert "payment" in response["body"]
-        assert "order_id" in response["body"]
+        status, response = self.dapp_order_manager.initiate_payment_against_order("order_test_123", 100, "USD", "paypal")
+        if status:
+            if "amount" in response:
+                assert "amount" in response["amount"]
+                assert "currency" in response["amount"]
+            else:
+                assert False
+            assert "payment_id" in response
+            assert "payment" in response
+            assert "order_id" in response
 
     def test_execute_order(self):
         pass
 
     def test_order_details(self):
         username = "123"
-        order_details = self.dapp_order_manager.get_order_details_for_user(username)
-        assert "body" in order_details
-        assert "headers" in order_details
-        assert "statusCode" in order_details
-        if order_details["statusCode"] == "success":
-            assert "orders" in order_details["body"]
+        status, order_details = self.dapp_order_manager.get_order_details_for_user(username)
+        if status:
+            assert "orders" in order_details
 
 
 if __name__ == "__main__":
--- payments/handlers/payment_handler.py
+++ payments/handlers/payment_handler.py
@@ -7,9 +7,10 @@ from common.constant import StatusCode
 
 def initiate(event, context):
     payload = json.loads(event['body'])
+    path_parameters = json.loads(event["pathParameters"])
     if validate_dict(payload, ["amount", "payment_method"]) \
-            and validate_dict(event['pathParameters'], ["order_id"]):
-        order_id = event['pathParameters']["order_id"]
+            and validate_dict(path_parameters, ["order_id"]):
+        order_id = path_parameters["order_id"]
         amount = payload["amount"]["amount"]
         currency = payload["amount"]["currency"]
         payment_method = payload["payment_method"]
@@ -33,10 +34,11 @@ def initiate(event, context):
 
 def execute(event, context):
     payload = json.loads(event['body'])
+    path_parameters = json.loads(event["pathParameters"])
     if validate_dict(payload, ["payment_method", "payment_details"]) \
-            and validate_dict(event["pathParameters"], ["order_id", "payment_id"]):
-        order_id = event['pathParameters']["order_id"]
-        payment_id = event['pathParameters']["payment_id"]
+            and validate_dict(path_parameters, ["order_id", "payment_id"]):
+        order_id = path_parameters["order_id"]
+        payment_id = path_parameters["payment_id"]
         payment_method = payload["payment_method"]
         payment_details = payload["payment_details"]
         status, response = OrderManager().execute_payment_against_order(order_id, payment_id, payment_details, payment_method)
@@ -58,9 +60,10 @@ def execute(event, context):
 
 
 def cancel(event, context):
-    if validate_dict(event['pathParameters'], ["order_id", "payment_id"]):
-        order_id = event['pathParameters']["order_id"]
-        payment_id = event['pathParameters']["payment_id"]
+    path_parameters = json.loads(event["pathParameters"])
+    if validate_dict(path_parameters, ["order_id", "payment_id"]):
+        order_id = path_parameters["order_id"]
+        payment_id = path_parameters["payment_id"]
         status, response = OrderManager().cancel_payment_against_order(order_id, payment_id)
         if status:
             return generate_lambda_response(
