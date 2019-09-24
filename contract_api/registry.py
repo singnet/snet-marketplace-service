0a29dc03ae26c5af72888a1321079bb140398031
anandrgitnirman <NIrman$123>
2019-09-24 14:55:41 +0530

    https://github.com/singnet/snet-marketplace-service/issues/420 Add organization_name as an additional atrribute along with the org_id for org_id filter

--- contract_api/registry.py
+++ contract_api/registry.py
@@ -264,8 +264,9 @@ class Registry:
                 filter_data = self.repo.execute("SELECT DISTINCT display_name AS ATTR_VALUE FROM service_metadata M, service S "
                                                 "WHERE S.row_id = M.service_row_id AND S.is_curated = 1")
             elif attribute == "org_id":
-                filter_data = self.repo.execute("SELECT DISTINCT organization_name AS ATTR_VALUE from organization O, service S "
+                filter_data = self.repo.execute("SELECT DISTINCT O.org_id ,O.organization_name from organization O, service S "
                                                 "WHERE S.org_id = O.org_id AND S.is_curated = 1")
+                return filter_data
             else:
                 return filter_attribute
             for rec in filter_data:
