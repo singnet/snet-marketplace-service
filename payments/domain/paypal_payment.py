1a4134e5740b228f524d2b145466666bfbbed0d8
Restyled.io <commits@restyled.io>
2019-09-24 13:26:15 +0000

    Restyled by yapf

--- payments/domain/paypal_payment.py
+++ payments/domain/paypal_payment.py
@@ -15,33 +15,34 @@ logger = get_logger(__name__)
 
 
 class PaypalPayment(Payment):
-    def __init__(
-        self, payment_id, amount, currency, payment_status, created_at, payment_details
-    ):
-        super().__init__(
-            payment_id, amount, currency, payment_status, created_at, payment_details
-        )
+    def __init__(self, payment_id, amount, currency, payment_status,
+                 created_at, payment_details):
+        super().__init__(payment_id, amount, currency, payment_status,
+                         created_at, payment_details)
         try:
-            self.payee_client_api = paypalrestsdk.Api(
-                {
-                    "mode": MODE,
-                    "client_id": get_ssm_parameter(PAYPAL_CLIENT),
-                    "client_secret": get_ssm_parameter(PAYPAL_SECRET),
-                }
-            )
+            self.payee_client_api = paypalrestsdk.Api({
+                "mode":
+                MODE,
+                "client_id":
+                get_ssm_parameter(PAYPAL_CLIENT),
+                "client_secret":
+                get_ssm_parameter(PAYPAL_SECRET),
+            })
         except Exception as e:
             logger.error("Failed to get ssm parameters")
             raise e
 
     def initiate_payment(self, order_id):
         paypal_payload = self.get_paypal_payload(order_id)
-        payment = paypalrestsdk.Payment(paypal_payload, api=self.payee_client_api)
+        payment = paypalrestsdk.Payment(paypal_payload,
+                                        api=self.payee_client_api)
 
         if not payment.create():
             logger.error(f"Paypal error:{payment.error}")
             raise Exception("Payment failed to create")
 
-        logger.info(f"Paypal payment initiated with paypal_payment_id: {payment.id}")
+        logger.info(
+            f"Paypal payment initiated with paypal_payment_id: {payment.id}")
 
         approval_url = ""
         for link in payment.links:
@@ -52,7 +53,12 @@ class PaypalPayment(Payment):
             raise Exception("Payment link not found")
         self._payment_details["payment_id"] = payment.id
 
-        response_payload = {"payment": {"id": payment.id, "payment_url": approval_url}}
+        response_payload = {
+            "payment": {
+                "id": payment.id,
+                "payment_url": approval_url
+            }
+        }
 
         return response_payload
 
@@ -60,9 +66,8 @@ class PaypalPayment(Payment):
         paypal_payment_id = self._payment_details["payment_id"]
         payer_id = paid_payment_details["payer_id"]
 
-        payment = paypalrestsdk.Payment.find(
-            paypal_payment_id, api=self.payee_client_api
-        )
+        payment = paypalrestsdk.Payment.find(paypal_payment_id,
+                                             api=self.payee_client_api)
 
         if payment.execute({"payer_id": payer_id}):
             logger.info(
@@ -82,28 +87,32 @@ class PaypalPayment(Payment):
 
     def get_paypal_payload(self, order_id):
         paypal_payload = {
-            "intent": "sale",
-            "payer": {"payment_method": PAYMENT_METHOD_PAYPAL},
-            "redirect_urls": {
-                "return_url": PAYMENT_RETURN_URL.format(order_id, self._payment_id),
-                "cancel_url": PAYMENT_CANCEL_URL.format(order_id, self._payment_id),
+            "intent":
+            "sale",
+            "payer": {
+                "payment_method": PAYMENT_METHOD_PAYPAL
             },
-            "transactions": [
-                {
-                    "item_list": {
-                        "items": [
-                            {
-                                "name": "item",
-                                "sku": "item",
-                                "price": self._amount,
-                                "currency": self._currency,
-                                "quantity": 1,
-                            }
-                        ]
-                    },
-                    "amount": {"total": self._amount, "currency": self._currency},
-                    "description": "",
-                }
-            ],
+            "redirect_urls": {
+                "return_url":
+                PAYMENT_RETURN_URL.format(order_id, self._payment_id),
+                "cancel_url":
+                PAYMENT_CANCEL_URL.format(order_id, self._payment_id),
+            },
+            "transactions": [{
+                "item_list": {
+                    "items": [{
+                        "name": "item",
+                        "sku": "item",
+                        "price": self._amount,
+                        "currency": self._currency,
+                        "quantity": 1,
+                    }]
+                },
+                "amount": {
+                    "total": self._amount,
+                    "currency": self._currency
+                },
+                "description": "",
+            }],
         }
         return paypal_payload
