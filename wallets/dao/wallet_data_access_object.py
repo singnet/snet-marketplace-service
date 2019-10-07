d66f18522ba28ca4092d960dc4999278524a2b52
Restyled.io <commits@restyled.io>
2019-10-07 11:18:00 +0000

    Restyled by black

--- wallets/dao/wallet_data_access_object.py
+++ wallets/dao/wallet_data_access_object.py
@@ -16,12 +16,16 @@ class WalletDAO:
             time_now = dt.utcnow()
             wallet_query = "INSERT INTO wallet (address, type, status, row_created, row_updated) VALUES (%s, %s, %s, %s, %s)"
             wallet_query_response = self.repo.execute(
-                wallet_query, [address, type, status, time_now, time_now])
+                wallet_query, [address, type, status, time_now, time_now]
+            )
 
-            user_wallet_query = "INSERT INTO user_wallet (username, address, is_default, row_created, row_updated) " \
-                                "VALUES (%s, %s, %s, %s, %s)"
+            user_wallet_query = (
+                "INSERT INTO user_wallet (username, address, is_default, row_created, row_updated) "
+                "VALUES (%s, %s, %s, %s, %s)"
+            )
             user_wallet_query_response = self.repo.execute(
-                user_wallet_query, [username, address, 1, time_now, time_now])
+                user_wallet_query, [username, address, 1, time_now, time_now]
+            )
             if user_wallet_query_response[0] == 1 and wallet_query_response[0] == 1:
                 self.repo.commit_transaction()
                 return True
@@ -33,7 +37,9 @@ class WalletDAO:
 
     def get_wallet_data_by_username(self, username):
         """ Method to get wallet details for a given username. """
-        query = "SELECT UW.address, UW.is_default, W.type, W.status " \
-                "FROM user_wallet as UW JOIN wallet as W ON UW.address = W.address WHERE UW.username= %s"
+        query = (
+            "SELECT UW.address, UW.is_default, W.type, W.status "
+            "FROM user_wallet as UW JOIN wallet as W ON UW.address = W.address WHERE UW.username= %s"
+        )
         wallet_data = self.repo.execute(query, username)
         return wallet_data
--- wallets/wallet_service.py
+++ wallets/wallet_service.py
@@ -5,7 +5,13 @@ from common.constant import TransactionStatus
 from common.logger import get_logger
 from common.ssm_utils import get_ssm_parameter
 from common.utils import Utils
-from wallets.config import NETWORK_ID, NETWORKS, SIGNER_ADDRESS, EXECUTOR_ADDRESS, EXECUTOR_KEY
+from wallets.config import (
+    NETWORK_ID,
+    NETWORKS,
+    SIGNER_ADDRESS,
+    EXECUTOR_ADDRESS,
+    EXECUTOR_KEY,
+)
 from wallets.constant import GENERAL_WALLET_TYPE, MPE_ADDR_PATH, MPE_CNTRCT_PATH
 from wallets.dao.channel_dao import ChannelDAO
 from wallets.wallet import Wallet
@@ -21,7 +27,7 @@ class WalletService:
         self.repo = obj_repo
         self.obj_blockchain_util = BlockChainUtil(
             provider_type="HTTP_PROVIDER",
-            provider=NETWORKS[NETWORK_ID]['http_provider']
+            provider=NETWORKS[NETWORK_ID]["http_provider"],
         )
         self.utils = Utils()
         self.channel_dao = ChannelDAO(obj_repo=self.repo)
@@ -30,9 +36,9 @@ class WalletService:
     def create_and_register_wallet(self, username):
         address, private_key = self.obj_blockchain_util.create_account()
         obj_wallet = Wallet(
-            address=address, private_key=private_key, type=GENERAL_WALLET_TYPE, status=0)
-        registered = self.register_wallet(
-            username=username, obj_wallet=obj_wallet)
+            address=address, private_key=private_key, type=GENERAL_WALLET_TYPE, status=0
+        )
+        registered = self.register_wallet(username=username, obj_wallet=obj_wallet)
         if registered:
             return obj_wallet.get_wallet()
         raise Exception("Unable to create and register wallet.")
@@ -44,7 +50,7 @@ class WalletService:
             username=username,
             address=wallet_details["address"],
             type=wallet_details["type"],
-            status=wallet_details["status"]
+            status=wallet_details["status"],
         )
 
         if persisted:
@@ -57,20 +63,43 @@ class WalletService:
         wallet_data = self.obj_wallet_dao.get_wallet_data_by_username(username)
         self.utils.clean(wallet_data)
 
-        logger.info(
-            f"Fetched {len(wallet_data)} wallets for username: {username}")
+        logger.info(f"Fetched {len(wallet_data)} wallets for username: {username}")
         wallet_response = {"username": username, "wallets": wallet_data}
         return wallet_response
 
-    def __generate_signature_details(self, recipient, group_id, agi_tokens, expiration, message_nonce, signer_key):
-        data_types = ["string", "address", "address", "address", "address", "bytes32", "uint256", "uint256",
-                      "uint256"]
-        values = ["__openChannelByThirdParty", self.mpe_address, EXECUTOR_WALLET_ADDRESS, SIGNER_ADDRESS, recipient,
-                  group_id, agi_tokens, expiration, message_nonce]
-        signature = self.obj_blockchain_util.generate_signature(data_types=data_types, values=values,
-                                                                signer_key=signer_key)
-        v, r, s = Web3.toInt(
-            hexstr="0x" + signature[-2:]), signature[:66], "0x" + signature[66:130]
+    def __generate_signature_details(
+        self, recipient, group_id, agi_tokens, expiration, message_nonce, signer_key
+    ):
+        data_types = [
+            "string",
+            "address",
+            "address",
+            "address",
+            "address",
+            "bytes32",
+            "uint256",
+            "uint256",
+            "uint256",
+        ]
+        values = [
+            "__openChannelByThirdParty",
+            self.mpe_address,
+            EXECUTOR_WALLET_ADDRESS,
+            SIGNER_ADDRESS,
+            recipient,
+            group_id,
+            agi_tokens,
+            expiration,
+            message_nonce,
+        ]
+        signature = self.obj_blockchain_util.generate_signature(
+            data_types=data_types, values=values, signer_key=signer_key
+        )
+        v, r, s = (
+            Web3.toInt(hexstr="0x" + signature[-2:]),
+            signature[:66],
+            "0x" + signature[66:130],
+        )
         return r, s, v, signature
 
     def __calculate_agi_tokens(self, amount, currency):
@@ -81,32 +110,48 @@ class WalletService:
 
         return agi_tokens
 
-    def open_channel_by_third_party(self, order_id, sender, sender_private_key, group_id, amount, currency, recipient):
+    def open_channel_by_third_party(
+        self,
+        order_id,
+        sender,
+        sender_private_key,
+        group_id,
+        amount,
+        currency,
+        recipient,
+    ):
         method_name = "openChannelByThirdParty"
         self.mpe_address = self.obj_blockchain_util.read_contract_address(
-            net_id=NETWORK_ID, path=MPE_ADDR_PATH,
-            key='address'
+            net_id=NETWORK_ID, path=MPE_ADDR_PATH, key="address"
         )
 
         current_block_no = self.obj_blockchain_util.get_current_block_no()
 
         # 1 block no is mined in 15 sec on average, setting expiration as 10 years
         expiration = current_block_no + (10 * 365 * 24 * 60 * 4)
-        agi_tokens = self.__calculate_agi_tokens(
-            amount=amount, currency=currency)
+        agi_tokens = self.__calculate_agi_tokens(amount=amount, currency=currency)
 
         group_id_in_hex = "0x" + base64.b64decode(group_id).hex()
         r, s, v, signature = self.__generate_signature_details(
-            recipient=recipient, group_id=group_id_in_hex,
-            agi_tokens=agi_tokens, expiration=expiration,
+            recipient=recipient,
+            group_id=group_id_in_hex,
+            agi_tokens=agi_tokens,
+            expiration=expiration,
             message_nonce=current_block_no,
-            signer_key=sender_private_key
+            signer_key=sender_private_key,
         )
 
         positional_inputs = (
-            sender, SIGNER_ADDRESS, recipient,
-            group_id_in_hex, agi_tokens, expiration,
-            current_block_no, v, r, s
+            sender,
+            SIGNER_ADDRESS,
+            recipient,
+            group_id_in_hex,
+            agi_tokens,
+            expiration,
+            current_block_no,
+            v,
+            r,
+            s,
         )
 
         transaction_object = self.obj_blockchain_util.create_transaction_object(
@@ -115,28 +160,36 @@ class WalletService:
             address=EXECUTOR_WALLET_ADDRESS,
             contract_path=MPE_CNTRCT_PATH,
             contract_address_path=MPE_ADDR_PATH,
-            net_id=NETWORK_ID
+            net_id=NETWORK_ID,
         )
 
         raw_transaction = self.obj_blockchain_util.sign_transaction_with_private_key(
-            transaction_object=transaction_object,
-            private_key=EXECUTOR_WALLET_KEY)
+            transaction_object=transaction_object, private_key=EXECUTOR_WALLET_KEY
+        )
         transaction_hash = self.obj_blockchain_util.process_raw_transaction(
-            raw_transaction=raw_transaction)
+            raw_transaction=raw_transaction
+        )
 
         print("openChannelByThirdParty::transaction_hash", transaction_hash)
 
         self.channel_dao.insert_channel_history(
-            order_id=order_id, amount=amount, currency=currency,
-            type=method_name, recipient=recipient,
-            address=sender, signature=signature,
+            order_id=order_id,
+            amount=amount,
+            currency=currency,
+            type=method_name,
+            recipient=recipient,
+            address=sender,
+            signature=signature,
             request_parameters=str(positional_inputs),
-            transaction_hash=transaction_hash, status=TransactionStatus.PENDING
+            transaction_hash=transaction_hash,
+            status=TransactionStatus.PENDING,
         )
 
         return {
-            "transaction_hash": transaction_hash, "signature": signature,
-            "agi_tokens": agi_tokens, "type": method_name
+            "transaction_hash": transaction_hash,
+            "signature": signature,
+            "agi_tokens": agi_tokens,
+            "type": method_name,
         }
 
     def update_wallet_status(self, address):
@@ -144,42 +197,42 @@ class WalletService:
 
     def add_funds_to_channel(self, order_id, channel_id, amount, currency):
         method_name = "channelAddFunds"
-        agi_tokens = self.__calculate_agi_tokens(
-            amount=amount, currency=currency)
+        agi_tokens = self.__calculate_agi_tokens(amount=amount, currency=currency)
         positional_inputs = (channel_id, agi_tokens)
 
         transaction_object = self.obj_blockchain_util.create_transaction_object(
-            *positional_inputs, method_name=method_name,
+            *positional_inputs,
+            method_name=method_name,
             address=EXECUTOR_WALLET_ADDRESS,
             contract_path=MPE_CNTRCT_PATH,
             contract_address_path=MPE_ADDR_PATH,
-            net_id=NETWORK_ID
+            net_id=NETWORK_ID,
         )
 
         raw_transaction = self.obj_blockchain_util.sign_transaction_with_private_key(
-            transaction_object=transaction_object,
-            private_key=EXECUTOR_WALLET_KEY)
+            transaction_object=transaction_object, private_key=EXECUTOR_WALLET_KEY
+        )
 
         transaction_hash = self.obj_blockchain_util.process_raw_transaction(
-            raw_transaction=raw_transaction)
+            raw_transaction=raw_transaction
+        )
         print("channelAddFunds::transaction_hash", transaction_hash)
         return {
-            "transaction_hash": transaction_hash, "agi_tokens": agi_tokens,
-            "positional_inputs": positional_inputs, "type": method_name
+            "transaction_hash": transaction_hash,
+            "agi_tokens": agi_tokens,
+            "positional_inputs": positional_inputs,
+            "type": method_name,
         }
 
     def get_transactions_from_username_recipient(self, username, recipient):
-        logger.info(
-            f"Fetching transactions for {username} to recipient: {recipient}")
+        logger.info(f"Fetching transactions for {username} to recipient: {recipient}")
         channel_data = self.channel_dao.get_channel_transactions_for_username_recipient(
-            username=username, recipient=recipient)
+            username=username, recipient=recipient
+        )
         self.utils.clean(channel_data)
 
         logger.info(f"Fetched {len(channel_data)} transactions")
-        transaction_details = {
-            "username": username,
-            "wallets": []
-        }
+        transaction_details = {"username": username, "wallets": []}
 
         wallet_transactions = dict()
         for rec in channel_data:
@@ -188,9 +241,9 @@ class WalletService:
                 wallet_transactions[rec["address"]] = {
                     "address": sender_address,
                     "is_default": rec["is_default"],
-                    "transactions": []
+                    "transactions": [],
                 }
-            if rec['recipient'] is None:
+            if rec["recipient"] is None:
                 continue
 
             transaction = {
@@ -201,8 +254,7 @@ class WalletService:
                 "created_at": rec["created_at"],
             }
 
-            wallet_transactions[sender_address]["transactions"].append(
-                transaction)
+            wallet_transactions[sender_address]["transactions"].append(transaction)
 
         for key in wallet_transactions:
             wallet = wallet_transactions[key]
@@ -211,13 +263,10 @@ class WalletService:
 
     def get_channel_transactions_against_order_id(self, order_id):
         transaction_history = self.channel_dao.get_channel_transactions_against_order_id(
-            order_id)
+            order_id
+        )
 
         for record in transaction_history:
-            record["created_at"] = record["created_at"].strftime(
-                "%Y-%m-%d %H:%M:%S")
+            record["created_at"] = record["created_at"].strftime("%Y-%m-%d %H:%M:%S")
 
-        return {
-            "order_id": order_id,
-            "transactions": transaction_history
-        }
+        return {"order_id": order_id, "transactions": transaction_history}
