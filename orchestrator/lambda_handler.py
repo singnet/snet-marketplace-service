c602d69f42a2c39732e76efe5f42147c45233e75
Restyled.io <commits@restyled.io>
2019-10-10 13:49:53 +0000

    Restyled by black

--- orchestrator/lambda_handler.py
+++ orchestrator/lambda_handler.py
@@ -12,10 +12,10 @@ from orchestrator.constant import REQUIRED_KEYS_FOR_LAMBDA_EVENT
 from orchestrator.order_service import OrderService
 from orchestrator.config import SLACK_HOOK
 
-NETWORKS_NAME = dict(
-    (NETWORKS[netId]["name"], netId) for netId in NETWORKS.keys())
-db = dict((netId, Repository(net_id=netId, NETWORKS=NETWORKS))
-          for netId in NETWORKS.keys())
+NETWORKS_NAME = dict((NETWORKS[netId]["name"], netId) for netId in NETWORKS.keys())
+db = dict(
+    (netId, Repository(net_id=netId, NETWORKS=NETWORKS)) for netId in NETWORKS.keys()
+)
 obj_util = Utils()
 
 
@@ -31,39 +31,45 @@ def route_path(path, method, payload_dict, request_context, path_parameters):
 
     elif "/order/initiate" == path:
         response_data = obj_order_service.initiate_order(
-            user_data=request_context, payload_dict=payload_dict)
+            user_data=request_context, payload_dict=payload_dict
+        )
 
     elif "/v2/order/initiate" == path:
         response_data = obj_order_service.initiate_order(
             user_data=request_context, payload_dict=payload_dict
         )
 
-    elif "/wallet/channel" == path and method == 'GET':
+    elif "/wallet/channel" == path and method == "GET":
         org_id = payload_dict["org_id"]
         username = request_context["authorizer"]["claims"]["email"]
         group_id = payload_dict["group_id"]
         response_data = obj_order_service.get_channel_details(
-            username, org_id, group_id)
+            username, org_id, group_id
+        )
 
     elif "/order/execute" == path and method == "POST":
         response_data = obj_order_service.execute_order(
-            user_data=request_context, payload_dict=payload_dict)
+            user_data=request_context, payload_dict=payload_dict
+        )
 
     elif re.match("(\/order\/)[^\/]*[/]{0,1}$", path):
         username = request_context["authorizer"]["claims"]["email"]
         order_id = path_parameters["order_id"]
         response_data = obj_order_service.get_order_details_by_order_id(
-            username=username, order_id=order_id)
+            username=username, order_id=order_id
+        )
 
     elif "/wallet/register" == path and method == "POST":
         username = request_context["authorizer"]["claims"]["email"]
         response_data = obj_order_service.register_wallet(
-            username=username, wallet_details=payload_dict)
+            username=username, wallet_details=payload_dict
+        )
 
     elif "/wallet/status" == path and method == "POST":
         username = request_context["authorizer"]["claims"]["email"]
         response_data = obj_order_service.set_default_wallet(
-            username=username, address=payload_dict["address"])
+            username=username, address=payload_dict["address"]
+        )
 
     else:
         path_exist = False
@@ -74,7 +80,8 @@ def route_path(path, method, payload_dict, request_context, path_parameters):
 def request_handler(event, context):
     try:
         valid_event = validate_dict(
-            data_dict=event, required_keys=REQUIRED_KEYS_FOR_LAMBDA_EVENT)
+            data_dict=event, required_keys=REQUIRED_KEYS_FOR_LAMBDA_EVENT
+        )
         if not valid_event:
             return generate_lambda_response(400, "Bad Request")
 
@@ -83,7 +90,8 @@ def request_handler(event, context):
         method = event["httpMethod"]
 
         method_found, path_parameters, payload_dict = extract_payload(
-            method=method, event=event)
+            method=method, event=event
+        )
         if not method_found:
             return generate_lambda_response(405, "Method Not Allowed")
 
@@ -92,7 +100,7 @@ def request_handler(event, context):
             method=method,
             payload_dict=payload_dict,
             request_context=event.get("requestContext", None),
-            path_parameters=path_parameters
+            path_parameters=path_parameters,
         )
         if not path_exist:
             return generate_lambda_response(404, "Not Found")
@@ -110,19 +118,14 @@ def request_handler(event, context):
         else:
             if "/v2/order/initiate" == path:
                 response = generate_lambda_redirect_response(
-                    302, {
-                        "status": "success",
-                        "data": response_data
-                    },
-                    headers={
-                        "location": response_data["payment"]["payment_url"]
-                    }
+                    302,
+                    {"status": "success", "data": response_data},
+                    headers={"location": response_data["payment"]["payment_url"]},
                 )
             else:
-                response = generate_lambda_response(200, {
-                    "status": "success",
-                    "data": response_data
-                })
+                response = generate_lambda_response(
+                    200, {"status": "success", "data": response_data}
+                )
     except Exception as e:
         error_message = format_error_message(
             status="failed",
