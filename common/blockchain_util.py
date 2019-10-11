42981146f2a2ded8aefc65c34023d9f4c70f7989
Restyled.io <commits@restyled.io>
2019-10-11 09:56:26 +0000

    Restyled by autopep8

--- common/blockchain_util.py
+++ common/blockchain_util.py
@@ -6,7 +6,6 @@ from eth_account.messages import defunct_hash_message
 from web3 import Web3
 
 
-
 class BlockChainUtil(object):
 
     def __init__(self, provider_type, provider):
@@ -15,7 +14,8 @@ class BlockChainUtil(object):
         elif provider_type == "WS_PROVIDER":
             self.provider = web3.providers.WebsocketProvider(provider)
         else:
-            raise Exception("Only HTTP_PROVIDER and WS_PROVIDER provider type are supported.")
+            raise Exception(
+                "Only HTTP_PROVIDER and WS_PROVIDER provider type are supported.")
         self.web3_object = Web3(self.provider)
 
     def load_contract(self, path):
@@ -32,9 +32,11 @@ class BlockChainUtil(object):
         return self.web3_object.eth.contract(abi=contract, address=address)
 
     def generate_signature(self, data_types, values, signer_key):
-        signer_key = "0x" + signer_key if not signer_key.startswith("0x") else signer_key
+        signer_key = "0x" + \
+            signer_key if not signer_key.startswith("0x") else signer_key
         message = web3.Web3.soliditySha3(data_types, values)
-        signature = self.web3_object.eth.account.signHash(defunct_hash_message(message), signer_key)
+        signature = self.web3_object.eth.account.signHash(
+            defunct_hash_message(message), signer_key)
         return signature.signature.hex()
 
     def get_nonce(self, address):
@@ -49,17 +51,19 @@ class BlockChainUtil(object):
                                   net_id):
         nonce = self.get_nonce(address=address)
         self.contract = self.load_contract(path=contract_path)
-        self.contract_address = self.read_contract_address(net_id=net_id, path=contract_address_path, key='address')
-        self.contract_instance = self.contract_instance(contract=self.contract, address=self.contract_address)
+        self.contract_address = self.read_contract_address(
+            net_id=net_id, path=contract_address_path, key='address')
+        self.contract_instance = self.contract_instance(
+            contract=self.contract, address=self.contract_address)
         print("gas_price == ", self.web3_object.eth.gasPrice)
         print("nonce == ", nonce)
         transaction_object = getattr(self.contract_instance.functions, method_name)(
             *positional_inputs).buildTransaction({
-            "from": address,
-            "nonce": nonce,
-            "gasPrice": self.web3_object.eth.gasPrice,
-            "chainId": 3
-        })
+                "from": address,
+                "nonce": nonce,
+                "gasPrice": self.web3_object.eth.gasPrice,
+                "chainId": 3
+            })
         return transaction_object
 
     def process_raw_transaction(self, raw_transaction):
@@ -73,4 +77,4 @@ class BlockChainUtil(object):
         return self.web3_object.eth.blockNumber
 
     def get_transaction_receipt_from_blockchain(self, transaction_hash):
-        return  self.web3_object.eth.getTransactionReceipt(transaction_hash)
+        return self.web3_object.eth.getTransactionReceipt(transaction_hash)
