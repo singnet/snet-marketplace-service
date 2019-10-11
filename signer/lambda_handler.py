6b4aadbb40ed6fbb9d239fdc8c8a8f5274a9bc45
Restyled.io <commits@restyled.io>
2019-10-11 12:37:37 +0000

    Restyled by reorder-python-imports

--- signer/lambda_handler.py
+++ signer/lambda_handler.py
@@ -3,8 +3,8 @@ import re
 import traceback
 
 from common.logger import get_logger
-from common.utils import Utils
 from common.utils import generate_lambda_response
+from common.utils import Utils
 from signer.config import NET_ID
 from signer.config import SLACK_HOOK
 from signer.signer import Signer
