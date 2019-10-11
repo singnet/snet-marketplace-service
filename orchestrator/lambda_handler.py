c20b386d19998b5c91994a8837f4a08e9f6923d7
Restyled.io <commits@restyled.io>
2019-10-11 10:24:23 +0000

    Restyled by autopep8

--- orchestrator/lambda_handler.py
+++ orchestrator/lambda_handler.py
@@ -14,7 +14,8 @@ from orchestrator.config import SLACK_HOOK
 
 NETWORKS_NAME = dict(
     (NETWORKS[netId]["name"], netId) for netId in NETWORKS.keys())
-db = dict((netId, Repository(net_id=netId, NETWORKS=NETWORKS)) for netId in NETWORKS.keys())
+db = dict((netId, Repository(net_id=netId, NETWORKS=NETWORKS))
+          for netId in NETWORKS.keys())
 obj_util = Utils()
 
 
@@ -36,7 +37,8 @@ def route_path(path, method, payload_dict, request_context, path_parameters):
         org_id = payload_dict["org_id"]
         username = request_context["authorizer"]["claims"]["email"]
         group_id = payload_dict["group_id"]
-        response_data = obj_order_service.get_channel_details(username, org_id, group_id)
+        response_data = obj_order_service.get_channel_details(
+            username, org_id, group_id)
 
     elif "/order/execute" == path and method == "POST":
         response_data = obj_order_service.execute_order(
@@ -45,15 +47,18 @@ def route_path(path, method, payload_dict, request_context, path_parameters):
     elif re.match("(\/order\/)[^\/]*[/]{0,1}$", path):
         username = request_context["authorizer"]["claims"]["email"]
         order_id = path_parameters["order_id"]
-        response_data = obj_order_service.get_order_details_by_order_id(username=username, order_id=order_id)
+        response_data = obj_order_service.get_order_details_by_order_id(
+            username=username, order_id=order_id)
 
     elif "/wallet/register" == path and method == "POST":
         username = request_context["authorizer"]["claims"]["email"]
-        response_data = obj_order_service.register_wallet(username=username, wallet_details=payload_dict)
+        response_data = obj_order_service.register_wallet(
+            username=username, wallet_details=payload_dict)
 
     elif "/wallet/status" == path and method == "POST":
         username = request_context["authorizer"]["claims"]["email"]
-        response_data = obj_order_service.set_default_wallet(username=username, address=payload_dict["address"])
+        response_data = obj_order_service.set_default_wallet(
+            username=username, address=payload_dict["address"])
 
     else:
         path_exist = False
@@ -96,7 +101,8 @@ def request_handler(event, context):
                 net_id=NETWORK_ID,
             )
             obj_util.report_slack(1, error_message, SLACK_HOOK)
-            response = generate_lambda_response(500, error_message, cors_enabled=True)
+            response = generate_lambda_response(
+                500, error_message, cors_enabled=True)
         else:
             response = generate_lambda_response(200, {
                 "status": "success",
@@ -111,6 +117,7 @@ def request_handler(event, context):
             net_id=NETWORK_ID,
         )
         obj_util.report_slack(1, error_message, SLACK_HOOK)
-        response = generate_lambda_response(500, error_message, cors_enabled=True)
+        response = generate_lambda_response(
+            500, error_message, cors_enabled=True)
         traceback.print_exc()
     return response
