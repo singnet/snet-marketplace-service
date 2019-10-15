cc131668668011c6ec0a9bc2aa1aaca107d1e87a
Restyled.io <commits@restyled.io>
2019-10-15 08:14:58 +0000

    Restyled by black

--- wallets/lambda_handler.py
+++ wallets/lambda_handler.py
@@ -3,17 +3,23 @@ from enum import Enum
 
 from common.logger import get_logger
 from common.repository import Repository
-from common.utils import Utils, generate_lambda_response, extract_payload, validate_dict, format_error_message
+from common.utils import (
+    Utils,
+    generate_lambda_response,
+    extract_payload,
+    validate_dict,
+    format_error_message,
+)
 from wallets.config import NETWORKS, NETWORK_ID, WALLET_TYPES_ALLOWED
 from wallets.config import SLACK_HOOK
 from wallets.constant import REQUIRED_KEYS_FOR_LAMBDA_EVENT
 from wallets.service.wallet_service import WalletService
 from wallets.wallet import Wallet
 
-NETWORKS_NAME = dict((NETWORKS[netId]['name'], netId)
-                     for netId in NETWORKS.keys())
-db = dict((netId, Repository(net_id=netId, NETWORKS=NETWORKS))
-          for netId in NETWORKS.keys())
+NETWORKS_NAME = dict((NETWORKS[netId]["name"], netId) for netId in NETWORKS.keys())
+db = dict(
+    (netId, Repository(net_id=netId, NETWORKS=NETWORKS)) for netId in NETWORKS.keys()
+)
 obj_util = Utils()
 logger = get_logger(__name__)
 
@@ -34,53 +40,61 @@ def route_path(path, method, payload_dict, path_parameters):
 
     elif "/wallet" == path and method == "GET":
         username = payload_dict["username"]
-        response_data = obj_wallet_manager.get_wallet_details(
-            username=username)
+        response_data = obj_wallet_manager.get_wallet_details(username=username)
 
-    elif "/wallet/channel" == path and method == 'POST':
-        response_data = obj_wallet_manager.open_channel_by_third_party(order_id=payload_dict['order_id'],
-                                                                       sender=payload_dict['sender'],
-                                                                       sender_private_key=payload_dict[
-                                                                           'sender_private_key'],
-                                                                       group_id=payload_dict['group_id'],
-                                                                       org_id=payload_dict["org_id"],
-                                                                       amount=payload_dict['amount'],
-                                                                       currency=payload_dict['currency'],
-                                                                       recipient=payload_dict['recipient'])
+    elif "/wallet/channel" == path and method == "POST":
+        response_data = obj_wallet_manager.open_channel_by_third_party(
+            order_id=payload_dict["order_id"],
+            sender=payload_dict["sender"],
+            sender_private_key=payload_dict["sender_private_key"],
+            group_id=payload_dict["group_id"],
+            org_id=payload_dict["org_id"],
+            amount=payload_dict["amount"],
+            currency=payload_dict["currency"],
+            recipient=payload_dict["recipient"],
+        )
 
-    elif "/wallet/channel/deposit" == path and method == 'POST':
-        response_data = obj_wallet_manager.add_funds_to_channel(org_id=payload_dict['org_id'],
-                                                                group_id=payload_dict['group_id'],
-                                                                channel_id=payload_dict['channel_id'],
-                                                                sender=payload_dict['sender'],
-                                                                recipient=payload_dict['recipient'],
-                                                                order_id=payload_dict['order_id'],
-                                                                amount=payload_dict['amount'],
-                                                                currency=payload_dict['currency'])
+    elif "/wallet/channel/deposit" == path and method == "POST":
+        response_data = obj_wallet_manager.add_funds_to_channel(
+            org_id=payload_dict["org_id"],
+            group_id=payload_dict["group_id"],
+            channel_id=payload_dict["channel_id"],
+            sender=payload_dict["sender"],
+            recipient=payload_dict["recipient"],
+            order_id=payload_dict["order_id"],
+            amount=payload_dict["amount"],
+            currency=payload_dict["currency"],
+        )
 
     elif "/wallet/status" == path:
-        response_data = obj_wallet_manager.set_default_wallet(username=payload_dict["username"],
-                                                              address=payload_dict['address'])
+        response_data = obj_wallet_manager.set_default_wallet(
+            username=payload_dict["username"], address=payload_dict["address"]
+        )
 
-    elif "/wallet/channel/transactions" == path and method == 'GET':
-        order_id = payload_dict.get('order_id', None)
-        username = payload_dict.get('username', None)
-        org_id = payload_dict.get('org_id', None)
-        group_id = payload_dict.get('group_id', None)
+    elif "/wallet/channel/transactions" == path and method == "GET":
+        order_id = payload_dict.get("order_id", None)
+        username = payload_dict.get("username", None)
+        org_id = payload_dict.get("org_id", None)
+        group_id = payload_dict.get("group_id", None)
 
         if order_id is not None:
             logger.info(
-                f"Received request to fetch transactions against order_id: {order_id}")
+                f"Received request to fetch transactions against order_id: {order_id}"
+            )
 
             response_data = obj_wallet_manager.get_channel_transactions_against_order_id(
-                order_id=payload_dict["order_id"])
+                order_id=payload_dict["order_id"]
+            )
         elif username is not None and group_id is not None and org_id is not None:
-            logger.info(f"Received request to fetch transactions for username: {username} "
-                        f"group_id: {group_id} "
-                        f"org_id: {org_id}")
+            logger.info(
+                f"Received request to fetch transactions for username: {username} "
+                f"group_id: {group_id} "
+                f"org_id: {org_id}"
+            )
 
             response_data = obj_wallet_manager.get_transactions_from_username_recipient(
-                username=username, group_id=group_id, org_id=org_id)
+                username=username, group_id=group_id, org_id=org_id
+            )
         else:
             raise Exception("Bad Parameters")
 
@@ -88,15 +102,13 @@ def route_path(path, method, payload_dict, path_parameters):
         username = payload_dict["username"]
         status = WalletStatus.ACTIVE.value
         wallet_address = payload_dict["address"]
-        wallet_type = payload_dict['type']
+        wallet_type = payload_dict["type"]
 
         if wallet_type not in WALLET_TYPES_ALLOWED:
             raise Exception("Wallet type invalid")
 
-        obj_wallet = Wallet(address=wallet_address, type=wallet_type,
-                            status=status)
-        obj_wallet_manager.register_wallet(
-            username=username, obj_wallet=obj_wallet)
+        obj_wallet = Wallet(address=wallet_address, type=wallet_type, status=status)
+        obj_wallet_manager.register_wallet(username=username, obj_wallet=obj_wallet)
         response_data = []
 
     else:
@@ -109,21 +121,24 @@ def request_handler(event, context):
     logger.info("Wallets::event: ", event)
     try:
         valid_event = validate_dict(
-            data_dict=event, required_keys=REQUIRED_KEYS_FOR_LAMBDA_EVENT)
+            data_dict=event, required_keys=REQUIRED_KEYS_FOR_LAMBDA_EVENT
+        )
         if not valid_event:
             return generate_lambda_response(400, "Bad Request")
 
-        path = event['path'].lower()
-        method = event['httpMethod']
+        path = event["path"].lower()
+        method = event["httpMethod"]
         method_found, path_parameters, payload_dict = extract_payload(
-            method=method, event=event)
+            method=method, event=event
+        )
         if not method_found:
             return generate_lambda_response(405, "Method Not Allowed")
 
         path_exist, response_data = route_path(
-            path=path, method=method,
+            path=path,
+            method=method,
             payload_dict=payload_dict,
-            path_parameters=path_parameters
+            path_parameters=path_parameters,
         )
 
         if not path_exist:
@@ -131,16 +146,27 @@ def request_handler(event, context):
         logger.info("Wallets::response_data: ", response_data)
 
         if response_data is None:
-            error_message = format_error_message(status="failed", error="Bad Request", resource=path,
-                                                 payload=payload_dict, net_id=NETWORK_ID)
+            error_message = format_error_message(
+                status="failed",
+                error="Bad Request",
+                resource=path,
+                payload=payload_dict,
+                net_id=NETWORK_ID,
+            )
             obj_util.report_slack(1, error_message, SLACK_HOOK)
             response = generate_lambda_response(500, error_message)
         else:
             response = generate_lambda_response(
-                200, {"status": "success", "data": response_data})
+                200, {"status": "success", "data": response_data}
+            )
     except Exception as e:
-        error_message = format_error_message(status="failed", error=repr(e), resource=path,
-                                             payload=payload_dict, net_id=NETWORK_ID)
+        error_message = format_error_message(
+            status="failed",
+            error=repr(e),
+            resource=path,
+            payload=payload_dict,
+            net_id=NETWORK_ID,
+        )
         obj_util.report_slack(1, error_message, SLACK_HOOK)
         response = generate_lambda_response(500, error_message)
         traceback.print_exc()
