3003b52c3297f396e7567e7ae3cd042dbe7e7969
prashantramangupta <you@example.com>
2019-10-17 01:32:21 +0530

    fix in orchestrator

--- signer/constant.py
+++ signer/constant.py
@@ -1,4 +1,4 @@
-COMMON_CNTRCT_PATH = '/signer/node_modules/singularitynet-platform-contracts'
+COMMON_CNTRCT_PATH = './signer/node_modules/singularitynet-platform-contracts'
 REG_CNTRCT_PATH = COMMON_CNTRCT_PATH + '/abi/Registry.json'
 MPE_CNTRCT_PATH = COMMON_CNTRCT_PATH + '/abi/MultiPartyEscrow.json'
 REG_ADDR_PATH = COMMON_CNTRCT_PATH + '/networks/Registry.json'
