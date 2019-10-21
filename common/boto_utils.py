e669d59539ba590a4fac62694959fbdc9d2f6581
Restyled.io <commits@restyled.io>
2019-10-21 11:37:59 +0000

    Restyled by black

--- common/boto_utils.py
+++ common/boto_utils.py
@@ -10,14 +10,20 @@ class BotoUtils:
 
     def get_ssm_parameter(self, parameter, config=Config()):
         """ Format config=Config(connect_timeout=1, read_timeout=0.1, retries={'max_attempts': 1}) """
-        ssm = boto3.client('ssm', region_name=self.region_name, config=config)
+        ssm = boto3.client("ssm", region_name=self.region_name, config=config)
         parameter = ssm.get_parameter(Name=parameter, WithDecryption=True)
         return parameter["Parameter"]["Value"]
 
-    def invoke_lambda(self, lambda_function_arn, invocation_type, payload, config=Config()):
+    def invoke_lambda(
+        self, lambda_function_arn, invocation_type, payload, config=Config()
+    ):
         """ Format config=Config(connect_timeout=1, read_timeout=0.1, retries={'max_attempts': 1}) """
         lambda_client = boto3.client(
-            'lambda', region_name=self.region_name, config=config)
-        lambda_response = lambda_client.invoke(FunctionName=lambda_function_arn, InvocationType=invocation_type,
-                                               Payload=payload)
-        return json.loads(lambda_response.get('Payload').read())
+            "lambda", region_name=self.region_name, config=config
+        )
+        lambda_response = lambda_client.invoke(
+            FunctionName=lambda_function_arn,
+            InvocationType=invocation_type,
+            Payload=payload,
+        )
+        return json.loads(lambda_response.get("Payload").read())
