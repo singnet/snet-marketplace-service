a7a0687091dcea868fcd6e2e390d751d9bc6e981
Restyled.io <commits@restyled.io>
2019-10-23 13:28:15 +0000

    Restyled by yapf

--- orchestrator/handlers/cancel_order_handler.py
+++ orchestrator/handlers/cancel_order_handler.py
@@ -23,20 +23,23 @@ logger = get_logger(__name__)
 def request_handler(event, context):
     try:
         valid_event = validate_dict(
-            data_dict=event, required_keys=REQUIRED_KEYS_FOR_CANCEL_ORDER_EVENT
-        )
+            data_dict=event,
+            required_keys=REQUIRED_KEYS_FOR_CANCEL_ORDER_EVENT)
         if not valid_event:
-            return generate_lambda_response(400, "Bad Request", cors_enabled=True)
+            return generate_lambda_response(400,
+                                            "Bad Request",
+                                            cors_enabled=True)
 
         path_parameters = event.get("pathParameters", None)
 
         order_service = OrderService(obj_repo=repo)
         cancel_order_status = order_service.cancel_order_for_given_order_id(
-            order_id=path_parameters["order_id"]
-        )
-        response = generate_lambda_response(
-            200, {"status": "success", "data": cancel_order_status}, cors_enabled=True
-        )
+            order_id=path_parameters["order_id"])
+        response = generate_lambda_response(200, {
+            "status": "success",
+            "data": cancel_order_status
+        },
+                                            cors_enabled=True)
     except Exception as e:
         error_message = format_error_message(
             status="failed",
@@ -46,6 +49,8 @@ def request_handler(event, context):
             handler="cancel_order_handler",
         )
         obj_util.report_slack(1, error_message, SLACK_HOOK)
-        response = generate_lambda_response(500, error_message, cors_enabled=True)
+        response = generate_lambda_response(500,
+                                            error_message,
+                                            cors_enabled=True)
         traceback.print_exc()
     return response
