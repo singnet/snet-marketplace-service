3a74a8e38630df030ad1096462030272c06318eb
Restyled.io <commits@restyled.io>
2019-10-24 10:36:44 +0000

    Restyled by yapf

--- contract_api/handlers/service_handlers.py
+++ contract_api/handlers/service_handlers.py
@@ -18,10 +18,10 @@ from contract_api.registry import Registry
 
 patch_all()
 
-NETWORKS_NAME = dict((NETWORKS[netId]["name"], netId) for netId in NETWORKS.keys())
-db = dict(
-    (netId, Repository(net_id=netId, NETWORKS=NETWORKS)) for netId in NETWORKS.keys()
-)
+NETWORKS_NAME = dict(
+    (NETWORKS[netId]["name"], netId) for netId in NETWORKS.keys())
+db = dict((netId, Repository(net_id=netId, NETWORKS=NETWORKS))
+          for netId in NETWORKS.keys())
 obj_util = Utils()
 
 net_id = NETWORK_ID
@@ -30,19 +30,24 @@ net_id = NETWORK_ID
 def get_service_get(event, context):
     obj_reg = Registry(obj_repo=db[net_id])
     payload_dict = event.get("queryStringParameters")
-    response_data = obj_reg.get_filter_attribute(attribute=payload_dict["attribute"])
-    return generate_lambda_response(
-        200, {"status": "success", "data": response_data}, cors_enabled=True
-    )
+    response_data = obj_reg.get_filter_attribute(
+        attribute=payload_dict["attribute"])
+    return generate_lambda_response(200, {
+        "status": "success",
+        "data": response_data
+    },
+                                    cors_enabled=True)
 
 
 def get_service_post(event, context):
     obj_reg = Registry(obj_repo=db[net_id])
     payload_dict = json.loads(event["body"])
     response_data = obj_reg.get_all_srvcs(qry_param=payload_dict)
-    return generate_lambda_response(
-        200, {"status": "success", "data": response_data}, cors_enabled=True
-    )
+    return generate_lambda_response(200, {
+        "status": "success",
+        "data": response_data
+    },
+                                    cors_enabled=True)
 
 
 def get_service_for_given_org(event, context):
@@ -50,28 +55,35 @@ def get_service_for_given_org(event, context):
     org_id = event["pathParameters"]["orgId"]
     service_id = event["pathParameters"]["serviceId"]
     response_data = obj_reg.get_service_data_by_org_id_and_service_id(
-        org_id=org_id, service_id=service_id
-    )
-    return generate_lambda_response(
-        200, {"status": "success", "data": response_data}, cors_enabled=True
-    )
+        org_id=org_id, service_id=service_id)
+    return generate_lambda_response(200, {
+        "status": "success",
+        "data": response_data
+    },
+                                    cors_enabled=True)
 
 
 def get_group_for_service(event, context):
     obj_reg = Registry(obj_repo=db[net_id])
     org_id = event["pathParameters"]["orgId"]
     service_id = event["pathParameters"]["serviceId"]
-    response_data = obj_reg.get_group_info(org_id=org_id, service_id=service_id)
-    return generate_lambda_response(
-        200, {"status": "success", "data": response_data}, cors_enabled=True
-    )
+    response_data = obj_reg.get_group_info(org_id=org_id,
+                                           service_id=service_id)
+    return generate_lambda_response(200, {
+        "status": "success",
+        "data": response_data
+    },
+                                    cors_enabled=True)
 
 
 def post_rating_for_given_service(event, context):
     obj_reg = Registry(obj_repo=db[net_id])
     org_id = event["pathParameters"]["orgId"]
     service_id = event["pathParameters"]["serviceId"]
-    response_data = obj_reg.update_service_rating(org_id=org_id, service_id=service_id)
-    return generate_lambda_response(
-        200, {"status": "success", "data": response_data}, cors_enabled=True
-    )
+    response_data = obj_reg.update_service_rating(org_id=org_id,
+                                                  service_id=service_id)
+    return generate_lambda_response(200, {
+        "status": "success",
+        "data": response_data
+    },
+                                    cors_enabled=True)
