871ea0a62f2eb6fe30b062dd561098d425d816ce
Sohit <sumitk002@gmail.com>
2019-10-24 20:08:24 +0530

    fix path param

--- contract_api/handlers/organization_handlers.py
+++ contract_api/handlers/organization_handlers.py
@@ -44,7 +44,7 @@ def get_all_group_for_org(event,context):
 def get_group_for_org_id(event,context):
     obj_reg = Registry(obj_repo=db[net_id])
     org_id = event['pathParameters']['orgId']
-    group_id = event['pathParameters']['groupId']
+    group_id = event['pathParameters']['group_id']
     response_data = obj_reg.get_group_details_for_org_id(org_id=org_id, group_id=group_id)
     return generate_lambda_response(
                 200, {"status": "success", "data": response_data},cors_enabled=True)
