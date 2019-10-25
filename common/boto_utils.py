12dce77e68984eb5b1ef4ae8ab827b52ed02e090
prashantramangupta <you@example.com>
2019-10-25 13:27:00 +0530

    boto max retries restricted to 1

--- common/boto_utils.py
+++ common/boto_utils.py
@@ -8,13 +8,13 @@ class BotoUtils:
     def __init__(self, region_name):
         self.region_name = region_name
 
-    def get_ssm_parameter(self, parameter, config=Config()):
+    def get_ssm_parameter(self, parameter, config=Config(retries={'max_attempts': 1})):
         """ Format config=Config(connect_timeout=1, read_timeout=0.1, retries={'max_attempts': 1}) """
         ssm = boto3.client('ssm', region_name=self.region_name, config=config)
         parameter = ssm.get_parameter(Name=parameter, WithDecryption=True)
         return parameter["Parameter"]["Value"]
 
-    def invoke_lambda(self, lambda_function_arn, invocation_type, payload, config=Config()):
+    def invoke_lambda(self, lambda_function_arn, invocation_type, payload, config=Config(retries={'max_attempts': 1})):
         """ Format config=Config(connect_timeout=1, read_timeout=0.1, retries={'max_attempts': 1}) """
         lambda_client = boto3.client('lambda', region_name=self.region_name, config=config)
         lambda_response = lambda_client.invoke(FunctionName=lambda_function_arn, InvocationType=invocation_type,
