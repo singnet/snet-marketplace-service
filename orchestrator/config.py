acd61356bf54421ca87d864ac13707efc4b2f764
Pratik <pratikap98@gmail.com>
2019-11-12 19:13:31 +0530

    change for circlerci

--- orchestrator/config.py
+++ orchestrator/config.py
@@ -5,9 +5,9 @@ NETWORKS = {
         "ws_provider": "wss://ropsten.infura.io/ws",
         "db": {
             "DB_HOST": "localhost",
-            "DB_USER": "unittest_root",
-            "DB_PASSWORD": "unittest_pwd",
-            "DB_NAME": "unittest_db",
+            "DB_USER": "root",
+            "DB_PASSWORD": "root",
+            "DB_NAME": "contract_index_test",
             "DB_PORT": 3306,
         },
     }
