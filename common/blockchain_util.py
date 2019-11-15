4848c87017dd74a7365e1de0a15f812c5b2c07eb
prashantramangupta <you@example.com>
2019-11-15 11:11:29 +0530

    excluding pending trxn while calculating nonce

--- common/blockchain_util.py
+++ common/blockchain_util.py
@@ -58,7 +58,7 @@ class BlockChainUtil(object):
 
     def get_nonce(self, address):
         """ transaction count includes pending transaction also. """
-        nonce = self.web3_object.eth.getTransactionCount(address, 'pending')
+        nonce = self.web3_object.eth.getTransactionCount(address)
         return nonce
 
     def sign_transaction_with_private_key(self, private_key, transaction_object):
