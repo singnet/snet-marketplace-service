9d9816ac3df7dd8cf036a34d50d8e8ccd1238b20
pratik-vii <pratikap98@gmail.com>
2019-12-10 14:05:43 +0530

    initial commit for the registry added serverless functions of organization in new module registry

---
+++ registry/application/handlers/organization_handlers.py
@@ -0,0 +1,24 @@
+
+
+def get_all_org(event, context):
+    pass
+
+
+def add_org(event, context):
+    pass
+
+
+def get_org(event, context):
+    pass
+
+
+def get_all_groups_for_org(event, context):
+    pass
+
+
+def add_group_for_org(event, context):
+    pass
+
+
+def get_group_for_org(event, context):
+    pass
