01f2e91f9c673500aee2d616b62f7d412fa56a80
pratik-vii <pratikap98@gmail.com>
2019-12-11 16:57:17 +0530

    fix for circleci database url added in config and alembic

---
+++ registry/config.py
@@ -0,0 +1,15 @@
+NETWORKS = {
+    0: {
+        "name": "test",
+        "http_provider": "https://ropsten.infura.io",
+        "ws_provider": "wss://ropsten.infura.io/ws",
+        "db": {
+            "DB_HOST": "localhost",
+            "DB_USER": "unittest_root",
+            "DB_PASSWORD": "unittest_pwd",
+            "DB_NAME": "unittest_db",
+            "DB_PORT": 3306,
+        },
+    }
+}
+NETWORK_ID = 0
