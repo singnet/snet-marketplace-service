efd99c123ccdb2f29691e165b59f437f16e4a9a7
Vivek205 <n.vin95@gmail.com>
2019-12-12 11:46:27 +0530

    added custom exception for BadRequest

--- orchestrator/handlers/verification_handler.py
+++ orchestrator/handlers/verification_handler.py
@@ -1,8 +1,9 @@
 from common.logger import get_logger
-from common.utils import handle_exception_with_slack_notification, generate_lambda_response
+from common.utils import handle_exception_with_slack_notification, generate_lambda_response, validate_dict
 from contract_api.config import NETWORK_ID, SLACK_HOOK
 from aws_xray_sdk.core import patch_all
 from orchestrator.services.verification_service import VerificationService
+from common.exceptions import BadRequestException
 
 patch_all()
 logger = get_logger(__name__)
@@ -12,6 +13,9 @@ Service = VerificationService()
 
 @handle_exception_with_slack_notification(logger=logger, SLACK_HOOK=SLACK_HOOK, NETWORK_ID=NETWORK_ID)
 def get_fields_handler(event, context):
+    required_keys = ["configurationName", "countryCode"]
+    if not validate_dict(event["pathParameters"], required_keys):
+        raise BadRequestException
     configuration_name = event["pathParameters"]["configurationName"]
     country_code = event["pathParameters"]["countryCode"]
     response = Service.get_fields(configuration_name, country_code)
