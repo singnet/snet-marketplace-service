eaddaf6ce394c22687010347faa3971aa56d9fd7
prashant <prashantramangupta@gmail.com>
2019-12-30 13:56:18 +0530

    addition of org address history table

---
+++ registry/alembic/versions/276e0d915625_create_organization_address_history_.py
@@ -0,0 +1,32 @@
+"""create organization address history table
+
+Revision ID: 276e0d915625
+Revises: 0a1eced60e7d
+Create Date: 2019-12-30 11:48:19.723884
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+# revision identifiers, used by Alembic.
+revision = '276e0d915625'
+down_revision = '0a1eced60e7d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('organization_address_history',
+                    sa.Column('row_id', sa.Integer(), nullable=False),
+                    sa.Column('organization_row_id', sa.Integer(), nullable=False),
+                    sa.Column('headquater_address', mysql.JSON(), nullable=False),
+                    sa.Column('mailing_address', mysql.JSON(), nullable=False),
+                    sa.Column('created_on', mysql.TIMESTAMP(), nullable=True),
+                    sa.Column('updated_on', mysql.TIMESTAMP(), nullable=True),
+                    sa.PrimaryKeyConstraint('row_id')
+                    )
+
+
+def downgrade():
+    op.drop_table('organization_address_history')
