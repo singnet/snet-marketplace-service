6b74848ac47fd44f009711c04ca87c3862446f4d
prashant <prashantramangupta@gmail.com>
2020-01-30 16:51:29 +0530

    base structure of service API's

---
+++ registry/application/services/service_publisher_service.py
@@ -0,0 +1,15 @@
+class ServicePublisherService:
+    def __init__(self):
+        pass
+
+    def verify_service_id(self, org_uuid, service_id):
+        pass
+
+    def save_service(self, org_uuid):
+        pass
+
+    def create_service(self, org_uuid):
+        pass
+
+    def get_services_for_organization(self, org_uuid):
+        pass
