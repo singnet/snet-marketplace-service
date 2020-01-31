5a58a903cbe8a463428f4523be60f3b8dc671624
pratik-vii <pratikap98@gmail.com>
2020-01-31 12:23:39 +0530

    added organization models for new changes in registry

--- registry/infrastructure/models/models.py
+++ registry/infrastructure/models/models.py
@@ -8,145 +8,89 @@ Base = declarative_base()
 
 class Organization(Base):
     __tablename__ = "organization"
-    row_id = Column("row_id", Integer, primary_key=True, autoincrement=True)
-    name = Column("name", VARCHAR(128), nullable=False)
-    org_uuid = Column("org_uuid", VARCHAR(128))
-    org_id = Column("org_id", VARCHAR(128), nullable=False)
-    owner_name = Column("owner_name", VARCHAR(256), nullable=False)
-    type = Column("type", VARCHAR(128), nullable=False)
-    owner = Column("owner", VARCHAR(128), nullable=False)
-    description = Column("description", VARCHAR(1024), nullable=False)
-    short_description = Column("short_description", VARCHAR(1024), nullable=False)
-    url = Column("url", VARCHAR(512), nullable=False)
-    duns_no = Column("duns_no", VARCHAR(20), nullable=True)
+    uuid = Column("uuid", VARCHAR(128), primary_key=True)
+    name = Column("name", VARCHAR(128))
+    org_id = Column("org_id", VARCHAR(128))
+    org_type = Column("org_type", VARCHAR(128))
     origin = Column("origin", VARCHAR(128))
+    description = Column("description", VARCHAR(1024))
+    short_description = Column("short_description", VARCHAR(1024))
+    url = Column("url", VARCHAR(512))
+    duns_no = Column("duns_no", VARCHAR(36))
     contacts = Column("contacts", JSON, nullable=False)
     assets = Column("assets", JSON, nullable=False)
     metadata_ipfs_hash = Column("metadata_ipfs_hash", VARCHAR(255))
+    org_state = relationship("OrganizationState", backref='organization', lazy='joined')
     groups = relationship("Group", backref='organization', lazy='joined')
-    address = relationship("OrganizationAddress", backref='organization', lazy='joined')
-
-
-class OrganizationReviewWorkflow(Base):
-    __tablename__ = "organization_review_workflow"
-    row_id = Column("row_id", Integer, primary_key=True, autoincrement=True)
-    org_row_id = Column("organization_row_id", Integer, nullable=False)
-    status = Column("status", VARCHAR(128), nullable=False)
-    transaction_hash = Column("transaction_hash", VARCHAR(128))
-    wallet_address = Column("user_address", VARCHAR(128))
-    created_by = Column("created_by", VARCHAR(128), nullable=False)
-    updated_by = Column("updated_by", VARCHAR(128), nullable=False)
-    approved_by = Column("approved_by", VARCHAR(128))
-    created_on = Column("created_on", TIMESTAMP(timezone=False))
-    updated_on = Column("updated_on", TIMESTAMP(timezone=False))
-
-
-class OrganizationHistory(Base):
-    __tablename__ = "organization_history"
-    row_id = Column("row_id", Integer, primary_key=True)
-    name = Column("name", VARCHAR(128), nullable=False)
-    org_uuid = Column("org_uuid", VARCHAR(128))
-    org_id = Column("org_id", VARCHAR(128), nullable=False)
-    owner_name = Column("owner_name", VARCHAR(256), nullable=False)
-    type = Column("type", VARCHAR(128), nullable=False)
-    owner = Column("owner", VARCHAR(128), nullable=False)
-    description = Column("description", VARCHAR(1024), nullable=False)
-    short_description = Column("short_description", VARCHAR(1024), nullable=False)
-    url = Column("url", VARCHAR(512), nullable=False)
-    duns_no = Column("duns_no", VARCHAR(20), nullable=True)
-    origin = Column("origin", VARCHAR(128))
-    contacts = Column("contacts", JSON, nullable=False)
-    assets = Column("assets", JSON, nullable=False)
-    metadata_ipfs_hash = Column("metadata_ipfs_hash", VARCHAR(255))
-    groups = relationship('GroupHistory', backref='organization_history', lazy='joined')
-    address = relationship('OrganizationAddressHistory', backref='organization_history', lazy='joined')
-
-
-class Group(Base):
-    __tablename__ = "group"
-    row_id = Column("row_id", Integer, primary_key=True, autoincrement=True)
-    name = Column("name", VARCHAR(128), nullable=False)
-    id = Column("id", VARCHAR(128), nullable=False)
-    org_uuid = Column("org_uuid", VARCHAR(128))
-    org_row_id = Column("org_row_id", Integer,
-                        ForeignKey("organization.row_id", ondelete="CASCADE", onupdate="CASCADE"),
-                        nullable=False)
-    payment_address = Column("payment_address", VARCHAR(128), nullable=False)
-    payment_config = Column("payment_config", JSON, nullable=False)
-    status = Column("status", VARCHAR(128))
-
-
-class GroupHistory(Base):
-    __tablename__ = "group_history"
-    row_id = Column("row_id", Integer, primary_key=True, autoincrement=True)
-    name = Column("name", VARCHAR(128), nullable=False)
-    id = Column("id", VARCHAR(128), nullable=False)
-    org_uuid = Column("org_uuid", VARCHAR(128))
-    org_row_id = Column("org_row_id", Integer,
-                        ForeignKey("organization_history.row_id", ondelete="CASCADE", onupdate="CASCADE"),
-                        nullable=False)
-    payment_address = Column("payment_address", VARCHAR(128), nullable=False)
-    payment_config = Column("payment_config", JSON, nullable=False)
-    status = Column("status", VARCHAR(128))
-
-
-class OrganizationMember(Base):
-    __tablename__ = "org_member"
-    row_id = Column("row_id", Integer, primary_key=True, autoincrement=True)
-    org_uuid = Column("org_uuid", VARCHAR(128))
-    role = Column("role", VARCHAR(128))
-    username = Column("username", VARCHAR(128))
-    address = Column("address", VARCHAR(128))
-    status = Column("status", VARCHAR(128))
-    transaction_hash = Column("transaction_hash", VARCHAR(128))
-    invite_code = Column("invite_code", VARCHAR(128))
-    invited_on = Column("invited_on", TIMESTAMP(timezone=False))
-    created_on = Column("created_on", TIMESTAMP(timezone=False))
-    updated_on = Column("updated_on", TIMESTAMP(timezone=False))
-    UniqueConstraint(org_uuid, username, name="uk_user_org")
+    addresses = relationship("OrganizationAddress", backref='organization', lazy='joined')
 
 
 class OrganizationAddress(Base):
     __tablename__ = "organization_address"
     row_id = Column("row_id", Integer, primary_key=True, autoincrement=True)
-    org_row_id = Column("org_row_id", Integer,
-                        ForeignKey("organization.row_id", ondelete="CASCADE", onupdate="CASCADE"),
-                        nullable=False)
+    org_uuid = Column("org_uuid", VARCHAR(128),
+                      ForeignKey("organization.uuid", ondelete="CASCADE", onupdate="CASCADE"),
+                      nullable=False)
     address_type = Column("address_type", VARCHAR(64), nullable=False)
     street_address = Column("street_address", VARCHAR(256), nullable=False)
     apartment = Column("apartment", VARCHAR(256), nullable=False)
     city = Column("city", VARCHAR(64), nullable=False)
-    pincode = Column("pincode", Integer, nullable=False)
+    pincode = Column("pincode", VARCHAR(64), nullable=False)
     state = Column("state", VARCHAR(64), nullable=True)
     country = Column("country", VARCHAR(64), nullable=False)
     created_on = Column("created_on", TIMESTAMP(timezone=False))
     updated_on = Column("updated_on", TIMESTAMP(timezone=False))
 
 
-class OrganizationAddressHistory(Base):
-    __tablename__ = "organization_address_history"
-    row_id = Column("row_id", Integer, primary_key=True)
-    org_row_id = Column("org_row_id", Integer,
-                        ForeignKey("organization_history.row_id", ondelete="CASCADE", onupdate="CASCADE"),
-                        nullable=False)
-    address_type = Column("address_type", VARCHAR(64), nullable=False)
-    street_address = Column("street_address", VARCHAR(256), nullable=False)
-    apartment = Column("apartment", VARCHAR(256), nullable=False)
-    city = Column("city", VARCHAR(64), nullable=False)
-    pincode = Column("pincode", Integer, nullable=False)
-    state = Column("state", VARCHAR(64), nullable=True)
-    country = Column("country", VARCHAR(64), nullable=False)
+class OrganizationState(Base):
+    __tablename__ = "organization_state"
+    row_id = Column("row_id", Integer, primary_key=True, autoincrement=True)
+    org_uuid = Column("org_uuid", VARCHAR(128),
+                      ForeignKey("organization.uuid", ondelete="CASCADE", onupdate="CASCADE"), nullable=False)
+    state = Column("state", VARCHAR(128), nullable=False)
+    transaction_hash = Column("transaction_hash", VARCHAR(128))
+    wallet_address = Column("user_address", VARCHAR(128))
+    created_by = Column("created_by", VARCHAR(128), nullable=False)
+    created_on = Column("created_on", TIMESTAMP(timezone=False))
+    updated_by = Column("updated_by", VARCHAR(128), nullable=False)
+    updated_on = Column("updated_on", TIMESTAMP(timezone=False))
+    reviewed_by = Column("approved_by", VARCHAR(128))
+    reviewed_on = Column("approved_on", TIMESTAMP(timezone=False))
+
+
+class OrganizationMember(Base):
+    __tablename__ = "org_member"
+    row_id = Column("row_id", Integer, autoincrement=True)
+    invite_code = Column("invite_code", VARCHAR(128), primary_key=True)
+    org_uuid = Column("org_uuid", VARCHAR(128),
+                      ForeignKey("organization.uuid", ondelete="CASCADE", onupdate="CASCADE"), nullable=False)
+    role = Column("role", VARCHAR(128))
+    username = Column("username", VARCHAR(128))
+    address = Column("address", VARCHAR(128))
+    status = Column("status", VARCHAR(128))
+    transaction_hash = Column("transaction_hash", VARCHAR(128))
+    invited_on = Column("invited_on", TIMESTAMP(timezone=False))
     created_on = Column("created_on", TIMESTAMP(timezone=False))
     updated_on = Column("updated_on", TIMESTAMP(timezone=False))
 
 
-# DB Model Class for Service
+class Group(Base):
+    __tablename__ = "group"
+    row_id = Column("row_id", Integer, autoincrement=True, primary_key=True)
+    name = Column("name", VARCHAR(128), nullable=False)
+    id = Column("id", VARCHAR(128), nullable=False)
+    org_uuid = Column("org_uuid", VARCHAR(128),
+                      ForeignKey("organization.uuid", ondelete="CASCADE", onupdate="CASCADE"), nullable=False)
+    payment_address = Column("payment_address", VARCHAR(128), nullable=False)
+    payment_config = Column("payment_config", JSON, nullable=False)
+    status = Column("status", VARCHAR(128))
+
 
 class Service(Base):
     __tablename__ = "service"
     row_id = Column("row_id", Integer, unique=True, autoincrement=True)
     org_uuid = Column("org_uuid", VARCHAR(128),
-                      ForeignKey("organization.org_uuid", ondelete="CASCADE", onupdate="CASCADE"),
+                      ForeignKey("organization.uuid", ondelete="CASCADE", onupdate="CASCADE"),
                       nullable=False)
     uuid = Column("uuid", VARCHAR(128), primary_key=True, nullable=False)
     display_name = Column("display_name", VARCHAR(128), nullable=False)
@@ -155,7 +99,7 @@ class Service(Base):
     proto = Column("proto", JSON, nullable=False)
     short_description = Column("short_description", VARCHAR(1024), nullable=False)
     description = Column("description", VARCHAR(1024), nullable=False)
-    git_url = Column("git_url", VARCHAR(512), nullable=False)
+    project_url = Column("git_url", VARCHAR(512), nullable=False)
     assets = Column("assets", JSON, nullable=False)
     rating = Column("ratings", JSON, nullable=False)
     ranking = Column("ranking", Integer, nullable=False, default=1)
@@ -168,10 +112,10 @@ class ServiceReviewWorkflow(Base):
     __tablename__ = "service_review_workflow"
     row_id = Column("row_id", Integer, primary_key=True, autoincrement=True)
     org_uuid = Column("org_uuid", VARCHAR(128), nullable=False)
-    service_uuid = Column("service_uuid", Integer,
+    service_uuid = Column("service_uuid", VARCHAR(128),
                           ForeignKey("service.uuid", ondelete="CASCADE", onupdate="CASCADE"),
                           unique=True, nullable=False)
-    status = Column("status", VARCHAR(128), nullable=False)
+    state = Column("state", VARCHAR(128), nullable=False)
     transaction_hash = Column("transaction_hash", VARCHAR(128))
     created_by = Column("created_by", VARCHAR(128), nullable=False)
     updated_by = Column("updated_by", VARCHAR(128), nullable=False)
@@ -185,7 +129,7 @@ class ServiceGroup(Base):
     __tablename__ = "service_group"
     row_id = Column("row_id", Integer, primary_key=True, autoincrement=True)
     org_uuid = Column("org_uuid", VARCHAR(128), nullable=False)
-    service_uuid = Column("service_uuid", Integer,
+    service_uuid = Column("service_uuid", VARCHAR(128),
                           ForeignKey("service.uuid", ondelete="CASCADE", onupdate="CASCADE"),
                           nullable=False)
     group_id = Column("group_id", VARCHAR(128), unique=True, nullable=False)
@@ -196,14 +140,13 @@ class ServiceGroup(Base):
     UniqueConstraint(org_uuid, service_uuid, group_id, name="uq_org_srvc_grp")
 
 
-# DB Model Class for Service Review History
 class ServiceReviewHistory(Base):
     __tablename__ = "service_review_history"
     row_id = Column("row_id", Integer, primary_key=True, autoincrement=True)
     org_uuid = Column("org_uuid", VARCHAR(128), nullable=False)
     service_uuid = Column("service_uuid", VARCHAR(128), nullable=False)
     reviewed_service_data = Column("reviewed_service_data", JSON, nullable=False)
-    status = Column("status", VARCHAR(64), nullable=False)
+    state = Column("state", VARCHAR(64), nullable=False)
     reviewed_by = Column("reviewed_by", VARCHAR(128), nullable=False)
     reviewed_on = Column("reviewed_on", TIMESTAMP(timezone=False))
     created_on = Column("created_on", TIMESTAMP(timezone=False))
