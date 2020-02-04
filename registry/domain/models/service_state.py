9dc2bd186a3fdd75f39e55ee7aae7fefb6ff957d
prashant <prashantramangupta@gmail.com>
2020-02-05 03:11:23 +0530

    service state entity model added

---
+++ registry/domain/models/service_state.py
@@ -0,0 +1,22 @@
+class ServiceState:
+    def __init__(self, org_uuid, service_uuid, state, transaction_hash):
+        self._org_uuid = org_uuid
+        self._service_uuid = service_uuid
+        self._state = state
+        self._transaction_hash = transaction_hash
+
+    def to_dict(self):
+        return {
+            "org_uuid": self._org_uuid,
+            "service_uuid": self._service_uuid,
+            "state": self._state,
+            "transaction_hash": self._transaction_hash
+        }
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def transaction_hash(self):
+        return self._transaction_hash
