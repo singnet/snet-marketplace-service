dd1b1a9ecbd44e2ee19654756ca260acd42ac844
pratik-vii <pratikap98@gmail.com>
2020-02-11 02:32:52 +0530

    handler for upload utility storing file to the temporary file location and getting file extension form content type, upload service call will store file to appropriate location and return URI as return value

--- upload_utility/application/handlers/upload_handler.py
+++ upload_utility/application/handlers/upload_handler.py
@@ -1,10 +1,13 @@
+import base64
+from uuid import uuid4
+
 from common.constant import StatusCode
 from common.exception_handler import exception_handler
 from common.logger import get_logger
-from common.utils import generate_lambda_response
+from common.utils import generate_lambda_response, validate_dict
 from upload_utility.application.upload_service import UploadService
-from upload_utility.config import SLACK_HOOK, NETWORK_ID, ALLOWED_CONTENT_TYPE
-from upload_utility.constants import UPLOAD_TYPE
+from upload_utility.config import SLACK_HOOK, NETWORK_ID
+from upload_utility.constants import UPLOAD_TYPE_DETAILS, ALLOWED_CONTENT_TYPE, FILE_EXTENSION, TEMP_FILE_DIR
 from upload_utility.exceptions import EXCEPTIONS, InvalidContentType, BadRequestException
 
 logger = get_logger(__name__)
@@ -13,14 +16,31 @@ logger = get_logger(__name__)
 @exception_handler(SLACK_HOOK=SLACK_HOOK, NETWORK_ID=NETWORK_ID, logger=logger, EXCEPTIONS=EXCEPTIONS)
 def upload_file(event, context):
     content_type = event["header"]["Content-Type"]
+    username = event["requestContext"]["authorizer"]["claims"]["email"]
     query_string_parameter = event["queryStringParameters"]
-    if content_type not in ALLOWED_CONTENT_TYPE:
+
+    if content_type not in ALLOWED_CONTENT_TYPE or len(event["body"]) == 0:
         InvalidContentType()
-    if query_string_parameter["type"] not in UPLOAD_TYPE:
+
+    upload_request_type = query_string_parameter["type"]
+    if upload_request_type not in UPLOAD_TYPE_DETAILS:
         BadRequestException()
 
-    response = UploadService().validate_and_store_file()
+    query_string_parameter.pop("type")
+    if not validate_dict(query_string_parameter, UPLOAD_TYPE_DETAILS[upload_request_type]):
+        BadRequestException()
+
+    file_extension = FILE_EXTENSION[content_type]
+    temp_file_path = f"{TEMP_FILE_DIR}/{uuid4().hex}_upload.{file_extension}"
+    raw_file_data = base64.b64decode(event["body"])
+    with open(temp_file_path, 'wb') as file:
+        file.write(raw_file_data)
+
+    response = UploadService().store_file(
+        upload_request_type, {"file_path": temp_file_path, "file_extension": file_extension},
+        query_string_parameter, username)
+
     return generate_lambda_response(
         StatusCode.OK,
-        {"status": "success", "data": response, "error": {}}, cors_enabled=True
+        {"status": "success", "data": {"url": response}, "error": {}}, cors_enabled=True
     )
