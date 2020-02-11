c6def763917970d59fb907262d8bafa425416f89
pratik-vii <pratikap98@gmail.com>
2020-02-11 15:38:24 +0530

    added content types to the config so we can change config and redeploy without code change

--- utility/config.py
+++ utility/config.py
@@ -25,3 +25,5 @@ SLACK_HOOK = {
 }
 REGION_NAME = ""
 ORG_BUCKET = ""
+ALLOWED_CONTENT_TYPE = ["application/zip", "image/jpg", "image/jpeg", "image/png"]
+FILE_EXTENSION = {"application/zip": 'zip', "image/jpg": "jpg", "image/jpeg": "jpeg", "image/png": "png"}
