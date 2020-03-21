dd08e44f20894797f3490aeb06087ae5296a3977
pratik-vii <pratikap98@gmail.com>
2020-03-21 18:20:22 +0530

    added new create organization API in orchestrator

--- orchestrator/publisher/application/services/organization_service.py
+++ orchestrator/publisher/application/services/organization_service.py
@@ -2,7 +2,8 @@ import json
 
 from common.boto_utils import BotoUtils
 from common.logger import get_logger
-from orchestrator.config import REGION_NAME, REGISTRY_ARN, WALLETS_SERVICE_ARN
+from orchestrator.config import REGION_NAME, REGISTRY_ARN, WALLETS_SERVICE_ARN, VERIFICATION_ARN
+from orchestrator.constant import VerificationType, OrganizationType
 
 logger = get_logger(__name__)
 
@@ -63,3 +64,54 @@ class OrganizationOrchestratorService:
         status = raw_response["statusCode"]
         if int(status) != 200:
             raise Exception("Unable to register wallet for username %s", username)
+
+    def create_and_initiate_verification(self, create_org_details, username):
+        org_details = self.create_org(create_org_details, username)
+        org_uuid = org_details["org_uuid"]
+        org_type = org_details["org_type"]
+        if org_type == OrganizationType.ORGANIZATION.value:
+            self.initiate_verification(org_uuid, VerificationType.DUNS.value, username)
+        elif org_type == OrganizationType.INDIVIDUAL.value:
+            self.initiate_verification(username, VerificationType.JUMIO.value, username)
+        else:
+            raise Exception(f"Verification initiate failed for Invalid org type {org_type}")
+        return org_details
+
+    def initiate_verification(self, entity_id, verification_type, username):
+        if verification_type == VerificationType.JUMIO.value:
+            initiate_verification_payload = {
+                "requestContext": {"authorizer": {"claims": {"email": entity_id}}},
+                "body": json.dumps({
+                    "type": verification_type
+                })
+            }
+        elif verification_type == VerificationType.DUNS.value:
+            initiate_verification_payload = {
+                "requestContext": {"authorizer": {"claims": {"email": username}}},
+                "body": json.dumps({
+                    "type": verification_type,
+                    "entity_id": entity_id
+                })
+            }
+        else:
+            raise Exception(f"Verification initiate failed for Invalid verification type {verification_type}")
+
+        raw_response = self.boto_client.invoke_lambda(lambda_function_arn=VERIFICATION_ARN["INITIATE"],
+                                                      invocation_type="RequestResponse",
+                                                      payload=json.dumps(initiate_verification_payload))
+        status = raw_response["statusCode"]
+        if status != 201:
+            raise Exception("Failed to create Organization")
+
+    def create_org(self, org_details, username):
+        create_organization_payload = {
+            "requestContext": {"authorizer": {"claims": {"email": username}}},
+            "body": json.dumps(org_details)
+        }
+        raw_response = self.boto_client.invoke_lambda(lambda_function_arn=REGISTRY_ARN["CREATE_ORG_ARN"],
+                                                      invocation_type="RequestResponse",
+                                                      payload=json.dumps(create_organization_payload))
+        status = raw_response["statusCode"]
+        if status != 201:
+            raise Exception("Failed to create Organization")
+        return json.loads(raw_response["body"])
