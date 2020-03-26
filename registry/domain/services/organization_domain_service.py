c97cf2d9d38fe6b47e0dc7aa4f522bd8408acf96
pratik-vii <pratikap98@gmail.com>
2020-03-26 10:14:56 +0530

    Revert "fix: arguments in getOrganizationById"
    This reverts commit e2c411a6

--- registry/domain/services/organization_domain_service.py
+++ registry/domain/services/organization_domain_service.py
@@ -30,7 +30,7 @@ class OrganizationService:
 
     def organization_exist_in_blockchain(self, org_id, contract, contract_address):
         method_name = "getOrganizationById"
-        positional_inputs = (web3.Web3.toHex(text=org_id),)
+        positional_inputs = (web3.Web3.toHex(text=org_id))
         contract = self.blockchain_util.contract_instance(contract_abi=contract, address=contract_address)
 
         org_data = self.blockchain_util.call_contract_function(contract=contract, contract_function=method_name,
@@ -93,8 +93,3 @@ class OrganizationService:
             return self.update_organization_in_blockchain(org_id, metadata_uri, environment)
         else:
             return self.register_organization_in_blockchain(org_id, metadata_uri, members, environment)
-
-
-p = OrganizationService().is_org_published("anand_17", "TEST")
-x = 0
-
