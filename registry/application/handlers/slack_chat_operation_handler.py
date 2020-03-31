c65430ce5660b446779ce143d8d8b57e1175627b
prashant <prashantramangupta@gmail.com>
2020-03-31 16:53:31 +0530

    code cleaning

--- registry/application/handlers/slack_chat_operation_handler.py
+++ registry/application/handlers/slack_chat_operation_handler.py
@@ -1,5 +1,5 @@
 from registry.application.services.slack_chat_operation import SlackChatOperation
-from registry.exceptions import BadRequestException, EXCEPTIONS
+from registry.exceptions import BadRequestException, EXCEPTIONS, InvalidSlackChannelException, InvalidSlackSignatureException, InvalidSlackUserException
 from registry.config import SLACK_HOOK, NETWORK_ID
 from urllib.parse import parse_qs
 from common.exception_handler import exception_handler
@@ -13,24 +13,26 @@ def get_list_of_service_pending_for_approval(event, context):
     event_body = event["body"]
     event_body_dict = parse_qs(event_body)
     headers = event["headers"]
+    logger.info(f"event_body_dict:: {event_body_dict}")
+    logger.info(f"headers:: {headers}")
 
     slack_chat_operation = SlackChatOperation(
         username=event_body_dict["user_name"][0], channel_id=event_body_dict["channel_id"][0])
 
     # validate slack channel
     if not slack_chat_operation.validate_slack_channel_id():
-        raise BadRequestException()
+        raise InvalidSlackChannelException()
 
     # validate slack user
     if not slack_chat_operation.validate_slack_user():
-        raise BadRequestException()
+        raise InvalidSlackUserException()
 
     # validate slack signature
     slack_signature_message = slack_chat_operation.generate_slack_signature_message(
         request_timestamp=headers["X-Slack-Request-Timestamp"], event_body=event_body)
     if not slack_chat_operation.validate_slack_signature(
             signature=headers["X-Slack-Signature"], message=slack_signature_message):
-        raise BadRequestException()
+        raise InvalidSlackSignatureException()
 
     # get services for given org_id
     return slack_chat_operation.get_list_of_service_pending_for_approval()
