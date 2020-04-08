71d55626ba792bfb25188fe563385203c1a5227b
Sohit <sumitk002@gmail.com>
2020-04-08 13:45:34 +0530

    wrapper for slack feedback

---
+++ utility/application/handlers/slack_feedback_handler.py
@@ -0,0 +1,18 @@
+import json
+
+import requests
+
+from common.utils import generate_lambda_response
+from utility.config import SLACK_HOOK
+
+
+def main(event, context):
+    username = event["requestContext"]["authorizer"]["claims"]["email"]
+    query_parameters = event["queryStringParameters"]
+
+    client = query_parameters['client']
+    slack_hook = SLACK_HOOK[client]
+    body = json.loads(event['body'])
+    body['username'] = username
+    requests.post(slack_hook, data=json.dumps(body))
+    generate_lambda_response(200, "OK")
