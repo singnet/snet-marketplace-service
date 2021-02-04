67026b0a87bd1c03abda5034c630c11d36a6177d
Vivek205 <ichbinvivek@gmail.com>
2021-02-04 11:03:05 +0530

    updated circle ci password

--- contract_api/config.py
+++ contract_api/config.py
@@ -6,9 +6,9 @@ NETWORKS = {
         "db": {
             "DB_DRIVER": "mysql+pymysql",
             "DB_HOST": "localhost",
-            "DB_USER": "root",
-            "DB_PASSWORD": "Admin@123",
-            "DB_NAME": "unittest_db",
+            "DB_USER": "unittest_root",
+            "DB_PASSWORD": "unittest_pwd",
+            "DB_NAME": "registry_unittest_db",
             "DB_PORT": 3306,
         },
     }
