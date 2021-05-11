9b5d21047e971d1bb1903a84547c7761e109879c
Rajeev <scraptorajeev@gmail.com>
2021-05-11 21:30:35 +0530

    Update service_factory.py

--- registry/domain/factory/service_factory.py
+++ registry/domain/factory/service_factory.py
@@ -26,7 +26,7 @@ class ServiceFactory:
             project_url=service.project_url,
             proto=service.proto,
             metadata_uri=service.metadata_uri,
-            assets=service.assets,
+            media=service.assets,
             rating=service.rating,
             ranking=service.ranking,
             contributors=service.contributors,
