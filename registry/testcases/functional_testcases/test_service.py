4f4247dc2f38d50761868a0e7607e68fbe775e46
Rajeev <scraptorajeev@gmail.com>
2021-05-13 09:47:38 +0530

    Update test_service.py

--- registry/testcases/functional_testcases/test_service.py
+++ registry/testcases/functional_testcases/test_service.py
@@ -1313,17 +1313,17 @@ class TestService(TestCase):
                                 "price_in_cogs": 1
                             }
                         ],
-                        "endpoints": {
-                            "https://example-service-a.singularitynet.io:8010": {
-                                "valid": False
-                            },
-                            "https://example-service-a.singularitynet.io:8013": {
-                                "valid": False
-                            },
-                            "https://example-service-a.singularitynet.io:8011": {
-                                "valid": True
-                            }
-                        },
+                "endpoints": {
+                                "https://example-service-a.singularitynet.io:8010": {
+                                  "valid": False
+                                },
+                                "https://example-service-a.singularitynet.io:8013": {
+                                  "valid": False
+                                },
+                                "https://example-service-a.singularitynet.io:8011": {
+                                  "valid": True
+                                }
+      },
                     }
                 ]
             })
@@ -1334,13 +1334,7 @@ class TestService(TestCase):
         assert (response_body["status"] == "success")
         assert (response_body["data"]["service_uuid"] == "test_service_uuid")
         assert (response_body["data"]["service_state"]["state"] == ServiceStatus.APPROVAL_PENDING.value)
-        assert (response_body["data"]['groups'] == [
-            {'group_id': 'l/hp6f1RXFPANeLWFZYwTB93Xi42S8NpZHfnceS6eUw=', 'group_name': 'defaultGroup',
-             'endpoints': {'https://example-service-a.singularitynet.io:8010': {'valid': False},
-                           'https://example-service-a.singularitynet.io:8013': {'valid': False},
-                           'https://example-service-a.singularitynet.io:8011': {'valid': True}}, 'test_endpoints': [],
-             'pricing': [{'default': True, 'price_model': 'fixed_price', 'price_in_cogs': 1}], 'free_calls': 15,
-             'free_call_signer_address': '0x7DF35C98f41F3Af0df1dc4c7F7D4C19a71Dd059F', 'daemon_addresses': []}])
+        assert (response_body["data"]['groups'] == [{'group_id': 'l/hp6f1RXFPANeLWFZYwTB93Xi42S8NpZHfnceS6eUw=', 'group_name': 'defaultGroup', 'endpoints': {'https://example-service-a.singularitynet.io:8010': {'valid': False}, 'https://example-service-a.singularitynet.io:8013': {'valid': False}, 'https://example-service-a.singularitynet.io:8011': {'valid': True}}, 'test_endpoints': [], 'pricing': [{'default': True, 'price_model': 'fixed_price', 'price_in_cogs': 1}], 'free_calls': 15, 'free_call_signer_address': '0x7DF35C98f41F3Af0df1dc4c7F7D4C19a71Dd059F', 'daemon_addresses': []}])
 
     def tearDown(self):
         org_repo.session.query(OrganizationStateDBModel).delete()
