3658c0dc92964024d1b746e23d41b68776570ce9
prashant <prashantramangupta@gmail.com>
2021-06-15 17:23:57 +0530

    Registry | Include organization state in create org API response.

--- registry/application/services/organization_publisher_service.py
+++ registry/application/services/organization_publisher_service.py
@@ -77,6 +77,7 @@ class OrganizationPublisherService:
             raise Exception("Org_id already exists")
         updated_state = Organization.next_state(None, None, OrganizationActions.CREATE.value)
         org_repo.add_organization(organization, self.username, updated_state)
+        organization = org_repo.get_organization(org_id=organization.id)
         return organization.to_response()
 
     def update_organization(self, payload, action):
