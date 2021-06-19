927b0a6d0a535d5e1425449c22670e2b7b7bcd78
Dhivakhar V <dhivakharjv@gmail.com>
2021-06-19 15:00:13 +0530

    Registry | Config update for test case

--- registry/config.py
+++ registry/config.py
@@ -6,8 +6,8 @@ NETWORKS = {
         "db": {
             "DB_DRIVER": "mysql+pymysql",
             "DB_HOST": "localhost",
-            "DB_USER": "root",
-            "DB_PASSWORD": "Dhiva@2025",
+            "DB_USER": "unittest_root",
+            "DB_PASSWORD": "unittest_pwd",
             "DB_NAME": "registry_unittest_db",
             "DB_PORT": 3306,
         },
