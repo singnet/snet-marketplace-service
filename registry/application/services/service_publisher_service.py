95c75b8013212663f84e516272287be7bda383a2
Dhivakhar V <dhivakharjv@gmail.com>
2021-07-21 11:29:44 +0530

    Registry | API to validate offchain_service_attributes

--- registry/application/services/service_publisher_service.py
+++ registry/application/services/service_publisher_service.py
@@ -1,7 +1,9 @@
 import json
 from uuid import uuid4
+import requests
 
 from aws_xray_sdk.core import patch_all
+from cerberus import Validator
 
 from common import utils
 from common.boto_utils import BotoUtils
@@ -10,16 +12,18 @@ from common.ipfs_util import IPFSUtil
 from common.logger import get_logger
 from common.utils import download_file_from_url, json_to_file, publish_zip_file_in_ipfs, send_email_notification
 from registry.config import ASSET_DIR, IPFS_URL, METADATA_FILE_PATH, NETWORKS, NETWORK_ID, NOTIFICATION_ARN, \
-    REGION_NAME
+    REGION_NAME, PUBLISH_OFFCHAIN_ATTRIBUTES_ENDPOINT
 from registry.constants import EnvironmentType, ServiceAvailabilityStatus, ServiceStatus, \
     ServiceSupportType, UserType
 from registry.domain.factory.service_factory import ServiceFactory
+from registry.domain.models.service import SERVICE_METADATA_SCHEMA, Service
 from registry.domain.models.service_comment import ServiceComment
 from registry.domain.services.service_publisher_domain_service import ServicePublisherDomainService
 from registry.exceptions import EnvironmentNotFoundException, InvalidServiceStateException, \
     OrganizationNotFoundException, ServiceNotFoundException, ServiceProtoNotFoundException
 from registry.infrastructure.repositories.organization_repository import OrganizationPublisherRepository
 from registry.infrastructure.repositories.service_publisher_repository import ServicePublisherRepository
+from deepdiff import DeepDiff
 
 patch_all()
 ALLOWED_ATTRIBUTES_FOR_SERVICE_SEARCH = ["display_name"]
@@ -35,6 +39,8 @@ BUILD_FAILURE_CODE = 0
 logger = get_logger(__name__)
 service_factory = ServiceFactory()
 boto_util = BotoUtils(region_name=REGION_NAME)
+validator = Validator()
+ipfs_util = IPFSUtil(IPFS_URL["url"], IPFS_URL["port"])
 
 
 class ServicePublisherService:
@@ -309,3 +315,75 @@ class ServicePublisherService:
             logger.info(
                 f"error in triggering build_id {build_id} for service {self._service_uuid} and org {self._org_uuid} :: error {repr(e)}")
             raise e
+
+    def publish_offchain_service_configs(self, org_id, service_id):
+        response = requests.post(
+            PUBLISH_OFFCHAIN_ATTRIBUTES_ENDPOINT.format(org_id.id, service_id))
+        if response.get["statusCode"] != 200:
+            raise Exception(f"Error in updating offchain service attributes")
+
+    def get_existing_service_details_from_contract_api(self, service_id, org_id):
+        payload = json.dumps({
+            "pathParameters": {
+                "serviceId": service_id,
+                "orgId": org_id
+            }
+        })
+        response = boto_util.invoke_lambda(
+            lambda_function_arn="",
+            invocation_type="RequestResponse",
+            payload=payload
+        )
+        logger.info(f"get existing service response :: {response}")
+        if response["statusCode"] == 200:
+            response = json.loads(response["body"])["data"]
+            return response
+        raise Exception(
+            f"Error getting service details from lambda for org_id :: {self._org_uuid} and service_id :: {self._service_uuid}")
+
+    def validate_service_metadata(self, current_organization_data, current_service_data, existing_service_data):
+        # VALIDATE METADATA
+        is_valid = Service.is_metadata_valid(service_metadata=current_service_data.to_metadata())
+        if not is_valid:
+            raise Exception("Service data is not valid!. Please make all valid data are present before publishing")
+        if not existing_service_data:
+            return {"publish_to_blockchain": True, "publish_offchain_attributes": True}
+        publish_onchain = publish_offchain = False
+        # ONCHAIN DIFF
+        existing_metadata = ipfs_util.read_file_from_ipfs(existing_service_data["ipfs_hash"])
+        onchain_diff = DeepDiff(current_service_data.to_metadata(), existing_metadata)
+        if onchain_diff:
+            publish_onchain = True
+            logger.info(f"changes in onchain_data :: f{onchain_diff}")
+        # OFFCHAIN DIFF
+        current_offchain_attributes = ServicePublisherRepository().get_offchain_service_config(
+            org_uuid=self._org_uuid,
+            service_uuid=self._service_uuid
+        )
+        offchain_diff = DeepDiff(current_offchain_attributes.configs,
+                                 existing_service_data["offchain_service_config"]["configs"])
+        if offchain_diff:
+            publish_offchain = True
+            logger.info(f"changes in onchain_data :: f{offchain_diff}")
+        return {"publish_to_blockchain": publish_onchain, "publish_offchain_attributes": publish_offchain}
+
+    def publish_service_data(self):
+        # Get required details for validation
+        current_organization_data = OrganizationPublisherRepository().get_organization(org_uuid=self._org_uuid)
+        current_service_data = ServicePublisherRepository() \
+            .get_service_for_given_service_uuid(org_uuid=self._org_uuid, service_uuid=self._service_uuid)
+        existing_service_data = self.get_existing_service_details_from_contract_api(current_service_data.service_id,
+                                                                                    current_organization_data.id)
+        # Validate metadata
+        service_validation = self.validate_service_metadata(current_organization_data, current_service_data,
+                                                            existing_service_data)
+        # Publish service data
+        if service_validation["publish_to_blockchain"]:
+            ipfs_data = self.publish_service_data_to_ipfs()
+            service_validation.update({"service_metadata_ipfs_hash": ipfs_data["service_metadata_ipfs_hash"]})
+        if service_validation["publish_offchain_attributes"]:
+            self.publish_offchain_service_configs(
+                org_id=current_organization_data.id,
+                service_id=current_service_data.service_id
+            )
+        return service_validation
