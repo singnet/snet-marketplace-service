7a4f1b181f18685e2785de2b1e0e62aefe217cd6
Dhivakhar V <dhivakharjv@gmail.com>
2021-08-04 21:37:24 +0530

    Wallet | clean up

--- wallets/domain/factory/wallet_factory.py
+++ wallets/domain/factory/wallet_factory.py
@@ -4,20 +4,18 @@ from wallets.domain.models.channel_transaction_history import ChannelTransaction
 class WalletFactory:
 
     @staticmethod
-    def convert_channel_transaction_history_db_model_to_entity_model(channel_transaction_history):
-        if not channel_transaction_history:
-            return None
+    def convert_channel_transaction_history_db_model_to_entity_model(channel_transaction_history_db):
         return ChannelTransactionHistory(
-            order_id=channel_transaction_history.order_id,
-            amount=channel_transaction_history.amount,
-            currency=channel_transaction_history.currency,
-            type=channel_transaction_history.type,
-            address=channel_transaction_history.address,
-            recipient=channel_transaction_history.recipient,
-            signature=channel_transaction_history.signature,
-            org_id=channel_transaction_history.org_id,
-            group_id=channel_transaction_history.group_id,
-            request_parameters=channel_transaction_history.request_parameters,
-            transaction_hash=channel_transaction_history.transaction_hash,
-            status=channel_transaction_history.status
+            order_id=channel_transaction_history_db.order_id,
+            amount=channel_transaction_history_db.amount,
+            currency=channel_transaction_history_db.currency,
+            type=channel_transaction_history_db.type,
+            address=channel_transaction_history_db.address,
+            recipient=channel_transaction_history_db.recipient,
+            signature=channel_transaction_history_db.signature,
+            org_id=channel_transaction_history_db.org_id,
+            group_id=channel_transaction_history_db.group_id,
+            request_parameters=channel_transaction_history_db.request_parameters,
+            transaction_hash=channel_transaction_history_db.transaction_hash,
+            status=channel_transaction_history_db.status
         )
