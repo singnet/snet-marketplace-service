e413a4ed851b531c2dc5164c3b6ba29c2d727d31
Dhivakhar V <dhivakharjv@gmail.com>
2021-08-06 14:20:24 +0530

    Wallet | PR comments

--- wallets/service/channel_transaction_status_service.py
+++ wallets/service/channel_transaction_status_service.py
@@ -1,12 +1,10 @@
 import json
 
 from common.blockchain_util import BlockChainUtil
+from common.boto_utils import BotoUtils
 from common.constant import TransactionStatus
 from common.logger import get_logger
-from wallets.dao.channel_transaction_status_data_access_object import \
-    ChannelTransactionStatusDataAccessObject
-from wallets.config import NETWORKS, NETWORK_ID, REGION_NAME, GET_MPE_PROCESSED_TRANSACTION_ARN
-from common.boto_utils import BotoUtils
+from wallets.config import NETWORKS, NETWORK_ID, REGION_NAME, GET_RAW_EVENT_DETAILS
 from wallets.infrastructure.repositories.channel_repository import ChannelRepository
 
 boto_utils = BotoUtils(region_name=REGION_NAME)
@@ -24,7 +22,7 @@ class ChannelTransactionStatusService:
     def get_mpe_processed_transactions_from_event_pub_sub(transaction_list):
         response = boto_utils.invoke_lambda(
             payload=json.dumps({"transaction_hash_list": transaction_list, "contract_name": "MPE"}),
-            lambda_function_arn=GET_MPE_PROCESSED_TRANSACTION_ARN,
+            lambda_function_arn=GET_RAW_EVENT_DETAILS,
             invocation_type="RequestResponse"
         )
         if response["statusCode"] != 200:
@@ -34,19 +32,16 @@ class ChannelTransactionStatusService:
     def manage_channel_transaction_status(self):
         # UPDATE PENDING TRANSACTIONS
         pending_txns_db = channel_repo.get_channel_transaction_history_data(status=TransactionStatus.PENDING)
-        logger.info(f"Pending transactions :: {[txn for txn in pending_txns_db]}")
         for txn_data in pending_txns_db:
             if txn_data.transaction_hash:
                 txn_receipt = obj_blockchain_util.get_transaction_receipt_from_blockchain(
                     transaction_hash=txn_data.transaction_hash)
-                txn_data.status = TransactionStatus.PROCESSING if txn_receipt.status == 1 else TransactionStatus.FAILED
-            else:
-                txn_data.status = TransactionStatus.NOT_SUBMITTED
-            channel_repo.update_channel_transaction_history_status_by_order_id(channel_txn_history=txn_data)
+                if txn_receipt:
+                    txn_data.status = TransactionStatus.PROCESSING if txn_receipt.status == 1 else TransactionStatus.FAILED
+                    channel_repo.update_channel_transaction_history_status_by_order_id(channel_txn_history=txn_data)
 
         # UPDATE PROCESSING TRANSACTIONS
         processing_txns_db = channel_repo.get_channel_transaction_history_data(status=TransactionStatus.PROCESSING)
-        logger.info(f"Processing transactions :: {[txn.transaction_hash for txn in processing_txns_db]}")
         processing_txn = [txn.transaction_hash for txn in processing_txns_db]
         if processing_txns_db:
             processed_transactions = self.get_mpe_processed_transactions_from_event_pub_sub(processing_txn)
