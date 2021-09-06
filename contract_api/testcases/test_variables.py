de339013299429ca1c6b1b88394d15bec6ee0103
Dhivakhar V <dhivakharjv@gmail.com>
2021-09-06 18:52:39 +0530

    Registry | Code smells cleanup

--- contract_api/testcases/test_variables.py
+++ contract_api/testcases/test_variables.py
@@ -1,25 +1,28 @@
+TEST_ORG_ID = "test_org"
+TEST_SERVICE_ID = "test_service"
+
 GET_SERVICE_RESPONSE = {
     "BASE": {'service_row_id': 10,
-             'org_id': 'snet',
-             'service_id': 'gene-annotation-service',
-             'display_name': 'Annotation Service',
-             'description': 'Use this service to annotate a humane genome with uniform terms, Reactome pathway memberships, and BioGrid protein interactions.',
-             'url': 'https://mozi-ai.github.io/annotation-service/',
-             'json': '{"name":"John", "age":31, "city":"New York"}',
-             'model_ipfs_hash': 'QmXqonxB9EvNBe11J8oCYXMQAtPKAb2x8CyFLmQpkvVaLf',
+             'org_id': TEST_ORG_ID,
+             'service_id': TEST_SERVICE_ID,
+             'display_name': 'test_display_name',
+             'description': 'test_description',
+             'url': 'test_url',
+             'json': '{"name":"test_name", "age":31, "city":"test_city"}',
+             'model_ipfs_hash': 'test_hash',
              'encoding': 'proto',
              'type': 'grpc',
-             'mpe_address': '0x8FB1dC8df86b388C7e00689d1eCb533A160B4D0C',
+             'mpe_address': 'test_mpe_address',
              'service_rating': {'rating': 0.0, 'total_users_rated': 0},
              'ranking': 1,
              'contributors': [{'name': 'dummy dummy', 'email_id': 'dummy@dummy.io'}],
              'short_description': 'short description',
              'demo_component_available': 0,
              'service_path': 'service_path',
-             'ipfs_hash': 'QmdGjaVYPMSGpC1qT3LDALSNCCu7JPf7j51H1GQirvQJYf',
+             'ipfs_hash': 'test_ipfs_hash',
              'is_curated': 1,
              'service_email': 'email',
-             'organization_name': 'gene-annotation-service',
+             'organization_name': TEST_SERVICE_ID,
              'owner_address': 'owner_add',
              'org_metadata_uri': 'uri',
              'org_email': 'email',
@@ -40,12 +43,11 @@ GET_SERVICE_RESPONSE = {
              },
     "MEDIA": {
         'media': [
-            {'service_row_id': 10, 'org_id': 'snet', 'service_id': 'gene-annotation-service',
-             'url': 'https://ropsten-service-components.s3.us-east-1.amazonaws.com/assets/d263/d263test/stubs/nodejs'
-                    '.zip',
+            {'service_row_id': 10, 'org_id': TEST_ORG_ID, 'service_id': TEST_SERVICE_ID,
+             'url': 'test_media_s3_url',
              'order': 0, 'file_type': 'grpc-stub/nodejs', 'asset_type': 'grpc-stub',
              'alt_text': ''},
-            {'service_row_id': 10, 'org_id': 'snet', 'service_id': 'gene-annotation-service',
+            {'service_row_id': 10, 'org_id': TEST_ORG_ID, 'service_id': TEST_SERVICE_ID,
              'url': 'https://test-s3-push', 'order': 5, 'file_type': 'image',
              'asset_type': 'hero_image', 'alt_text': 'data is missing'}
         ]}
--- contract_api/testcases/unit_testcases/test_registry.py
+++ contract_api/testcases/unit_testcases/test_registry.py
@@ -6,7 +6,7 @@ from contract_api.infrastructure.models import ServiceMedia, OffchainServiceConf
 from contract_api.infrastructure.repositories.service_media_repository import ServiceMediaRepository
 from contract_api.infrastructure.repositories.service_repository import OffchainServiceConfigRepository
 from contract_api.registry import Registry
-from contract_api.testcases.test_variables import GET_SERVICE_RESPONSE
+from contract_api.testcases.test_variables import GET_SERVICE_RESPONSE, TEST_ORG_ID, TEST_SERVICE_ID
 
 db = Repository(net_id=NETWORK_ID, NETWORKS=NETWORKS)
 service_media_repo = ServiceMediaRepository()
@@ -17,21 +17,21 @@ modified_date = "2021-08-19 00:01:20"
 class TestRegistry(TestCase):
     def test_curation(self):
         registry = Registry(obj_repo=db)
-        insert_service_query = """INSERT INTO service (org_id,service_id,service_path,ipfs_hash,is_curated,service_email
-        ,row_created,row_updated) VALUES ('snet','freecall',NULL,'QmQtm73kmKhv6mKTkn7qW3uMPtgK6c5Qytb11sCxY98s5j',0,
+        insert_service_query = f"""INSERT INTO service (org_id,service_id,service_path,ipfs_hash,is_curated,service_email
+        ,row_created,row_updated) VALUES ('{TEST_ORG_ID}','{TEST_SERVICE_ID}',NULL,'QmQtm73kmKhv6mKTkn7qW3uMPtgK6c5Qytb11sCxY98s5j',0,
         NULL,'2019-08-23 07:00:31','2020-03-18 13:07:55');"""
         db.execute(insert_service_query)
-        registry.curate_service('snet', 'freecall', True)
+        registry.curate_service(TEST_ORG_ID, TEST_SERVICE_ID, True)
         service_details = db.execute("SELECT is_curated FROM service where service_id=%s and org_id=%s",
-                                     ['freecall', 'snet'])
+                                     [TEST_SERVICE_ID, TEST_ORG_ID])
         if len(service_details) > 0:
             assert service_details[0]['is_curated'] == 1
         else:
             assert False
 
-        registry.curate_service('snet', 'freecall', False)
+        registry.curate_service(TEST_ORG_ID, TEST_SERVICE_ID, False)
         service_details = db.execute("SELECT is_curated FROM service where service_id=%s and org_id=%s",
-                                     ['freecall', 'snet'])
+                                     [TEST_SERVICE_ID, TEST_ORG_ID])
         if len(service_details) > 0:
             assert service_details[0]['is_curated'] == 0
         else:
@@ -41,27 +41,27 @@ class TestRegistry(TestCase):
         registry = Registry(obj_repo=db)
         self.tearDown()
 
-        insert_organization_query = """INSERT INTO organization
+        insert_organization_query = f"""INSERT INTO organization
         (row_id ,org_id, organization_name, owner_address, org_metadata_uri, org_email, org_assets_url, row_created, row_updated, description, assets_hash, contacts)
-        VALUES(10,'snet', 'gene-annotation-service', 'owner_add', 'uri', 'email', '{"url":"google.com"}', '2021-01-08 05:48:26', '2021-01-08 05:48:26', 'description', '{}','{}');"""
+        VALUES(10,'{TEST_ORG_ID}', '{TEST_SERVICE_ID}', """ + """'owner_add', 'uri', 'email', '{"url":"google.com"}', '2021-01-08 05:48:26', '2021-01-08 05:48:26', 'description', '{}','{}');"""
         db.execute(insert_organization_query)
 
-        insert_service_query = """INSERT INTO service
+        insert_service_query = f"""INSERT INTO service
         (row_id ,org_id, service_id, service_path, ipfs_hash, is_curated, service_email, row_created, row_updated)
-        VALUES(10,'snet', 'gene-annotation-service', 'service_path', 'QmdGjaVYPMSGpC1qT3LDALSNCCu7JPf7j51H1GQirvQJYf', 1, 'email', '2021-01-08 05:48:26', '2021-01-08 05:48:26');"""
+        VALUES(10,'{TEST_ORG_ID}', '{TEST_SERVICE_ID}', """ + """ 'service_path', 'test_ipfs_hash', 1, 'email', '2021-01-08 05:48:26', '2021-01-08 05:48:26');"""
         db.execute(insert_service_query)
 
-        insert_metadata_query = """INSERT INTO service_metadata
+        insert_metadata_query = f"""INSERT INTO service_metadata
         (row_id ,service_row_id, org_id, service_id, display_name, description, short_description,demo_component_available, url, json, model_ipfs_hash, encoding, `type`, mpe_address, assets_url, assets_hash, service_rating, ranking, contributors, row_created, row_updated)
-        VALUES(10,10, 'snet', 'gene-annotation-service', 'Annotation Service', 'Use this service to annotate a humane genome with uniform terms, Reactome pathway memberships, and BioGrid protein interactions.', 'short description',0, 'https://mozi-ai.github.io/annotation-service/', '{"name":"John", "age":31, "city":"New York"}', 'QmXqonxB9EvNBe11J8oCYXMQAtPKAb2x8CyFLmQpkvVaLf', 'proto', 'grpc', '0x8FB1dC8df86b388C7e00689d1eCb533A160B4D0C','{"hero_image": "https://test-s3-push"}', '{"hero_image": "QmVcE6fEDP764ibadXTjZHk251Lmt5xAxdc4P9mPA4kksk/hero_gene-annotation-2b.png"}','{"rating": 0.0, "total_users_rated": 0}', 1, '[{"name": "dummy dummy", "email_id": "dummy@dummy.io"}]', '2021-01-08 05:48:26', '2021-01-08 05:48:26')"""
+        VALUES(10,10, '{TEST_ORG_ID}', '{TEST_SERVICE_ID}', """ + """ 'test_display_name', 'test_description', 'short description',0, 'test_url', '{"name":"test_name", "age":31, "city":"test_city"}', 'test_hash', 'proto', 'grpc', 'test_mpe_address','{"hero_image": "https://test-s3-push"}', '{"hero_image": "test_hero_image_hash"}','{"rating": 0.0, "total_users_rated": 0}', 1, '[{"name": "dummy dummy", "email_id": "dummy@dummy.io"}]', '2021-01-08 05:48:26', '2021-01-08 05:48:26')"""
         db.execute(insert_metadata_query)
 
         service_media_repo.add_item(ServiceMedia(
             row_id=8,
             service_row_id=10,
-            org_id="snet",
-            service_id="gene-annotation-service",
-            url="https://ropsten-service-components.s3.us-east-1.amazonaws.com/assets/d263/d263test/stubs/nodejs.zip",
+            org_id=TEST_ORG_ID,
+            service_id=TEST_SERVICE_ID,
+            url="test_media_s3_url",
             order=0,
             asset_type="grpc-stub",
             file_type="grpc-stub/nodejs",
@@ -74,8 +74,8 @@ class TestRegistry(TestCase):
         service_media_repo.add_item(ServiceMedia(
             row_id=10,
             service_row_id=10,
-            org_id="snet",
-            service_id="gene-annotation-service",
+            org_id=TEST_ORG_ID,
+            service_id=TEST_SERVICE_ID,
             url="https://test-s3-push",
             order=5,
             asset_type="hero_image",
@@ -86,39 +86,39 @@ class TestRegistry(TestCase):
             updated_on="2021-01-08 13:31:50"
         ))
 
-        response = registry.get_service_data_by_org_id_and_service_id('snet', 'gene-annotation-service')
+        response = registry.get_service_data_by_org_id_and_service_id(TEST_ORG_ID, TEST_SERVICE_ID)
 
         result = {}
         result.update(GET_SERVICE_RESPONSE["BASE"])
         result.update(GET_SERVICE_RESPONSE["MEDIA"])
-        assert response == result
+        self.assertDictEqual(response, result)
 
         offchain_service_repo.add_item(OffchainServiceConfigDB(
-            org_id="snet",
-            service_id="gene-annotation-service",
+            org_id=TEST_ORG_ID,
+            service_id=TEST_SERVICE_ID,
             parameter_name="demo_component_url",
             parameter_value="sample_demo_url",
             created_on=modified_date,
             updated_on=modified_date,
         ))
         offchain_service_repo.add_item(OffchainServiceConfigDB(
-            org_id="snet",
-            service_id="gene-annotation-service",
+            org_id=TEST_ORG_ID,
+            service_id=TEST_SERVICE_ID,
             parameter_name="demo_component_status",
             parameter_value="PENDING",
             created_on=modified_date,
             updated_on=modified_date,
         ))
         offchain_service_repo.add_item(OffchainServiceConfigDB(
-            org_id="snet",
-            service_id="gene-annotation-service",
+            org_id=TEST_ORG_ID,
+            service_id=TEST_SERVICE_ID,
             parameter_name="demo_component_required",
             parameter_value=1,
             created_on=modified_date,
             updated_on=modified_date,
         ))
 
-        response = registry.get_service_data_by_org_id_and_service_id('snet', 'gene-annotation-service')
+        response = registry.get_service_data_by_org_id_and_service_id(TEST_ORG_ID, TEST_SERVICE_ID)
 
         result = {}
         result.update(GET_SERVICE_RESPONSE["BASE"])
@@ -130,30 +130,31 @@ class TestRegistry(TestCase):
                                           },
                        "demo_component_required": 1
                        })
-        assert response == result
+        self.assertDictEqual(response, result)
 
     def test_get_service_data_by_org_id_and_service_id_without_media(self):
         registry = Registry(obj_repo=db)
         self.tearDown()
 
-        insert_organization_query = """INSERT INTO organization
+        insert_organization_query = f"""INSERT INTO organization
         (row_id ,org_id, organization_name, owner_address, org_metadata_uri, org_email, org_assets_url, row_created, row_updated, description, assets_hash, contacts)
-        VALUES(10,'snet', 'gene-annotation-service', 'owner_add', 'uri', 'email', '{"url":"google.com"}', '2021-01-08 05:48:26', '2021-01-08 05:48:26', 'description', '{}','{}');"""
+        VALUES(10, '{TEST_ORG_ID}', '{TEST_SERVICE_ID}', """ + """ 'owner_add', 'uri', 'email', '{"url":"google.com"}', '2021-01-08 05:48:26', '2021-01-08 05:48:26', 'description', '{}','{}');"""
         db.execute(insert_organization_query)
 
-        insert_service_query = """INSERT INTO service
+        insert_service_query = f"""INSERT INTO service
         (row_id ,org_id, service_id, service_path, ipfs_hash, is_curated, service_email, row_created, row_updated)
-        VALUES(10,'snet', 'gene-annotation-service', 'service_path', 'QmdGjaVYPMSGpC1qT3LDALSNCCu7JPf7j51H1GQirvQJYf', 1, 'email', '2021-01-08 05:48:26', '2021-01-08 05:48:26');"""
+        VALUES(10,  '{TEST_ORG_ID}', '{TEST_SERVICE_ID}', """ + """'service_path', 'test_ipfs_hash',
+         1, 'email', '2021-01-08 05:48:26', '2021-01-08 05:48:26');"""
         db.execute(insert_service_query)
 
         # intentionally omitted the value for demo_component_available inorder to test the default value
-        insert_metadata_query = """INSERT INTO service_metadata
+        insert_metadata_query = f"""INSERT INTO service_metadata
         (row_id ,service_row_id, org_id, service_id, display_name, description, short_description, url, json, model_ipfs_hash, encoding, `type`, mpe_address, assets_url, assets_hash, service_rating, ranking, contributors, row_created, row_updated)
-        VALUES(10,10, 'snet', 'gene-annotation-service', 'Annotation Service', 'Use this service to annotate a humane genome with uniform terms, Reactome pathway memberships, and BioGrid protein interactions.', 'short description', 'https://mozi-ai.github.io/annotation-service/', '{"name":"John", "age":31, "city":"New York"}', 'QmXqonxB9EvNBe11J8oCYXMQAtPKAb2x8CyFLmQpkvVaLf', 'proto', 'grpc', '0x8FB1dC8df86b388C7e00689d1eCb533A160B4D0C','{"hero_image": "https://test-s3-push"}', '{"hero_image": "QmVcE6fEDP764ibadXTjZHk251Lmt5xAxdc4P9mPA4kksk/hero_gene-annotation-2b.png"}','{"rating": 0.0, "total_users_rated": 0}', 1, '[{"name": "dummy dummy", "email_id": "dummy@dummy.io"}]', '2021-01-08 05:48:26', '2021-01-08 05:48:26')"""
+        VALUES(10,10,  '{TEST_ORG_ID}', '{TEST_SERVICE_ID}', """ + """ 'test_display_name', 'test_description', 'short description', 'test_url', '{"name":"test_name", "age":31, "city":"test_city"}', 'test_hash', 'proto', 'grpc', 'test_mpe_address','{"hero_image": "https://test-s3-push"}', '{"hero_image": "test_hero_image_hash"}','{"rating": 0.0, "total_users_rated": 0}', 1, '[{"name": "dummy dummy", "email_id": "dummy@dummy.io"}]', '2021-01-08 05:48:26', '2021-01-08 05:48:26')"""
         db.execute(insert_metadata_query)
 
-        response = registry.get_service_data_by_org_id_and_service_id('snet', 'gene-annotation-service')
-        assert response == GET_SERVICE_RESPONSE["BASE"]
+        response = registry.get_service_data_by_org_id_and_service_id(TEST_ORG_ID, TEST_SERVICE_ID)
+        self.assertDictEqual(response, GET_SERVICE_RESPONSE["BASE"])
 
     def tearDown(self):
         db.execute("DELETE FROM service WHERE 1")
