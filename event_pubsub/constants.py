5d534230c31ba3931605b4971a08dd392699a289
karthi96 <karthikeyandon105@gmail.com>
2022-04-20 07:51:16 +0530

    adding bridge contract from npm package

--- event_pubsub/constants.py
+++ event_pubsub/constants.py
@@ -10,3 +10,12 @@ class EventType(Enum):
     OCCAM_SNET_AIRDROP = 'OCCAM_SNET_AIRDROP'
     CONVERTER_AGIX = 'CONVERTER_AGIX'
     CONVERTER_NTX = 'CONVERTER_NTX'
+
+
+class NodeModulesPackagePath(Enum):
+    BRIDGE = "node_modules/singularitynet-bridge"
+
+
+class TokenSymbol(Enum):
+    AGIX = "AGIX"
+    NTX = "NTX"
