b3913038f50e3a43de6a328f07493d8a2d03a6fc
Kirill Dvoretskov <65371121+kiruxaspb@users.noreply.github.com>
2024-04-22 12:51:09 +0300

    Revert "fix change display name for update service (#1793)" (#1796)
    This reverts commit 024470f3ebcef2b0c52e4f158ef70c87641dd8e0.

--- registry/application/services/service_publisher_service.py
+++ registry/application/services/service_publisher_service.py
@@ -128,7 +128,6 @@ class ServicePublisherService:
         service = ServicePublisherRepository().get_service_for_given_service_uuid(self._org_uuid, self._service_uuid)
         service.service_id = payload["service_id"]
         service.proto = payload.get("proto", {})
-        service.display_name = payload.get("display_name", "")
         service.short_description = payload.get("short_description", "")
         service.description = payload.get("description", "")
         service.project_url = payload.get("project_url", "")
