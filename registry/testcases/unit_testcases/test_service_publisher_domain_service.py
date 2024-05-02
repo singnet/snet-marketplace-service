e5d025c1705a4d6441f8b2d3c0849f3cbcd2ee50
vbakatov <vitaly.bakatov@singularitynet.io>
2024-05-02 17:11:53 +0300

    [SPT-284] update test for get_service_metadata method

--- registry/testcases/unit_testcases/test_service_publisher_domain_service.py
+++ registry/testcases/unit_testcases/test_service_publisher_domain_service.py
@@ -10,35 +10,52 @@ class TestServicePublisherDomainService(TestCase):
 
     def test_get_service_metadata(self):
         service = ServiceEntityModel(
-            org_uuid="test_org_uuid", uuid="test_service_uuid", service_id="test_service_id",
-            display_name="test_display_name", short_description="test_short_description",
-            description="test_description", project_url="https://dummy.io",
+            org_uuid="test_org_uuid",
+            uuid="test_service_uuid",
+            service_id="test_service_id",
+            display_name="test_display_name",
+            short_description="test_short_description",
+            description="test_description",
+            project_url="https://dummy.io",
             proto={"encoding": "proto", "service_type": "grpc", "model_ipfs_hash": "test_model_ipfs_hash"},
             assets={
                 "proto_files": {
                     "url": "https://ropsten-marketplace-service-assets.s3.amazonaws.com/9887ec2e099e4afd92c4a052737eaa"
                            "97/services/7420bf47989e4afdb1797d1bba8090aa/proto/20200327130256_proto_files.zip",
-                    "ipfs_hash": "QmUfDprFisFeaRnmLEqks1AFN6iam5MmTh49KcomXHEiQY"},
+                    "ipfs_hash": "QmUfDprFisFeaRnmLEqks1AFN6iam5MmTh49KcomXHEiQY"
+                },
                 "hero_image": {
                     "url": "QmUfDprFisFeaRnmLEqks1AFN6iam5MmTh49KcomXHEiQY/20200323130126_asset.png",
-                    "ipfs_hash": "QmUfDprFisFeaRnmLEqks1AFN6iam5MmTh49KcomXHEiQY"},
+                    "ipfs_hash": "QmUfDprFisFeaRnmLEqks1AFN6iam5MmTh49KcomXHEiQY"
+                },
                 "demo_files": {
                     "url": "https://ropsten-marketplace-service-assets.s3.amazonaws.com/9887ec2e099e4afd92c4a052737eaa"
                            "97/services/7420bf47989e4afdb1797d1bba8090aa/component/20200401121414_component.zip",
                     "ipfs_hash": "QmUfDprFisFeaRnmLEqks1AFN6iam5MmTh49KcomXHEiQY"
-                }},
+                }
+            },
             ranking=1,
-            rating={}, contributors=[{'name': 'df', 'email_id': '123@mail.io'}], tags=["1234", "4321"],
+            rating={},
+            contributors=[{'name': 'df', 'email_id': '123@mail.io'}], tags=["1234", "4321"],
             mpe_address="0x123",
-            metadata_uri="", service_type="grpc", groups=[
+            metadata_uri="",
+            service_type="grpc",
+            groups=[
                 ServiceGroup(
-                    org_uuid="test_org_uuid", service_uuid="test_service_uuid",
-                    group_id="test", group_name="test_name",
+                    org_uuid="test_org_uuid",
+                    service_uuid="test_service_uuid",
+                    group_id="test",
+                    group_name="test_name",
                     endpoints={"https://dummydaemonendpoint.io": {"verfied": True}},
                     test_endpoints=["https://dummydaemonendpoint.io"],
                     pricing=[{'default': True, 'price_model': 'fixed_price', 'price_in_cogs': 1}],
-                    free_calls=10, daemon_address=["0xq2w3e4rr5t6y7u8i9"],
-                    free_call_signer_address="0xq2s3e4r5t6y7u8i9o0")], service_state=None
+                    free_calls=10,
+                    daemon_address=["0xq2w3e4rr5t6y7u8i9"],
+                    free_call_signer_address="0xq2s3e4r5t6y7u8i9o0"
+                )
+            ],
+            service_state=None,
+            trainig_indicator=False
         )
         service_metadata = ServicePublisherDomainService("", "", "").get_service_metadata(service,
                                                                                           EnvironmentType.MAIN.value)
