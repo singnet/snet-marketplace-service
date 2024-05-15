acb249d742b2ba4953e895e7de1d0b387e38c0ad
vbakatov <vitaly.bakatov@singularitynet.io>
2024-05-15 13:07:31 +0300

    [FIX] add logs

--- utility/application/handlers/proto_stubs_handler.py
+++ utility/application/handlers/proto_stubs_handler.py
@@ -19,8 +19,10 @@ def manage_proto_compilation(event, context):
     service_id = event['service_id']
     response = GenerateStubService(). \
         manage_proto_compilation(input_s3_path=input_s3_path, output_s3_path=output_s3_path, org_id=org_id, service_id=service_id)
-    return generate_lambda_response(StatusCode.OK, {"status": "success", "data": response, "error": {}},
+    lambda_response = generate_lambda_response(StatusCode.OK, {"status": "success", "data": response, "error": {}},
                                     cors_enabled=True)
+    logger.debug(f"Creating lambda response :: {lambda_response}")
+    return lambda_response
 
 
 @exception_handler(SLACK_HOOK=SLACK_HOOK, NETWORK_ID=NETWORK_ID, logger=logger, EXCEPTIONS=EXCEPTIONS)
@@ -31,5 +33,7 @@ def generate_grpc_python_stubs(event, context):
     org_id = event['org_id']
     service_id = event['service_id']
     response = generate_python_stubs(input_s3_path=input_s3_path, output_s3_path=output_s3_path, org_id=org_id, service_id=service_id)
-    return generate_lambda_response(StatusCode.OK, {"status": "success", "data": response, "error": {}},
+    lambda_response = generate_lambda_response(StatusCode.OK, {"status": "success", "data": response, "error": {}},
                                     cors_enabled=True)
+    logger.debug(f"Creating lambda response :: {lambda_response}")
+    return lambda_response
