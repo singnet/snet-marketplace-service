a573347cdc58c929fd9dd0c571c27a725e4b2644
Arondondon <andreypravdin52@gmail.com>
2025-02-06 11:11:06 +0300

    Disabled sql logs

--- registry/infrastructure/repositories/base_repository.py
+++ registry/infrastructure/repositories/base_repository.py
@@ -13,14 +13,12 @@ port = DB_DETAILS["port"]
 db_name = DB_DETAILS["name"]
 
 connection_string = f"{driver}://{user}:{password}@{host}:{port}/{db_name}"
-engine = create_engine(connection_string, pool_pre_ping=True, echo=True)
+engine = create_engine(connection_string, pool_pre_ping=True, echo=False)
 
 Session = sessionmaker(bind=engine)
 default_session = Session()
 # get_logger("sqlalchemy.engine").setLevel("INFO")
 # get_logger("sqlalchemy.pool").setLevel("DEBUG")
-get_logger("sqlalchemy.engine").setLevel(logging.NOTSET)
-get_logger("sqlalchemy.pool").setLevel(logging.NOTSET)
 
 
 class BaseRepository:
