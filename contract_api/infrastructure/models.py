627cf11c86b79134ceff2a3439d02077a2416ee3
Arondondon <andreypravdin52@gmail.com>
2025-07-01 14:34:03 +0300

    feat: add migration

--- contract_api/infrastructure/models.py
+++ contract_api/infrastructure/models.py
@@ -18,11 +18,11 @@ class MpeChannel(Base):
     recipient: Mapped[str] = mapped_column("recipient", VARCHAR(128), nullable=False)
     group_id: Mapped[str] = mapped_column("groupId", VARCHAR(128), nullable=False)
     balance_in_cogs: Mapped[int] = mapped_column(
-        "balance_in_cogs", DECIMAL(38, 0), default=null
+        "balance_in_cogs", DECIMAL(38, 0), nullable=True, default=null
     )
-    nonce: Mapped[int] = mapped_column("nonce", Integer, default=null)
-    expiration: Mapped[int] = mapped_column("expiration", BIGINT, default=null)
-    pending: Mapped[int] = mapped_column("pending", DECIMAL(38, 0), default=null)
+    nonce: Mapped[int] = mapped_column("nonce", Integer, nullable=True, default=null)
+    expiration: Mapped[int] = mapped_column("expiration", BIGINT, nullable=True, default=null)
+    pending: Mapped[int] = mapped_column("pending", DECIMAL(38, 0), nullable=True, default=null)
     consumed_balance: Mapped[int] = mapped_column(
         "consumed_balance", DECIMAL(38, 0), default=0
     )
@@ -52,9 +52,9 @@ class OrgGroup(Base):
         nullable=False,
         index=True
     )
-    group_id: Mapped[str] = mapped_column("group_id", VARCHAR(256), default=null)
-    group_name: Mapped[str] = mapped_column("group_name", VARCHAR(128), default=null)
-    payment: Mapped[dict] = mapped_column("payment", JSON, default=null)
+    group_id: Mapped[str] = mapped_column("group_id", VARCHAR(256), nullable=True, default=null)
+    group_name: Mapped[str] = mapped_column("group_name", VARCHAR(128), nullable=True, default=null)
+    payment: Mapped[dict] = mapped_column("payment", JSON, nullable=False, default={})
 
     created_on: Mapped[datetime] = mapped_column(
         "created_on", TIMESTAMP(timezone=False), nullable=False, server_default=func.now()
@@ -76,15 +76,15 @@ class Organization(Base):
     __tablename__ = "organization"
     row_id: Mapped[int] = mapped_column("row_id", Integer, primary_key=True, autoincrement=True)
     org_id: Mapped[str] = mapped_column("org_id", VARCHAR(128), nullable=False, unique=True)
-    organization_name: Mapped[str] = mapped_column("organization_name", VARCHAR(128), default=null)
-    owner_address: Mapped[str] = mapped_column("owner_address", VARCHAR(256), default=null)
-    org_metadata_uri: Mapped[str] = mapped_column("org_metadata_uri", VARCHAR(128), default=null)
-    org_email: Mapped[str] = mapped_column("org_email", VARCHAR(128), default=null)
-    org_assets_url: Mapped[dict] = mapped_column("org_assets_url", JSON, default={})
-    is_curated: Mapped[bool]  = mapped_column("is_curated", BOOLEAN, default=null)
-    description: Mapped[dict] = mapped_column("description", JSON, default={})
-    assets_hash: Mapped[dict] = mapped_column("assets_hash", JSON, default={})
-    contacts: Mapped[dict] = mapped_column("contacts", JSON, default={})
+    organization_name: Mapped[str] = mapped_column("organization_name", VARCHAR(128), nullable=True, default=null)
+    owner_address: Mapped[str] = mapped_column("owner_address", VARCHAR(256), nullable=True, default=null)
+    org_metadata_uri: Mapped[str] = mapped_column("org_metadata_uri", VARCHAR(128), nullable=True, default=null)
+    org_email: Mapped[str] = mapped_column("org_email", VARCHAR(128), nullable=True, default=null)
+    org_assets_url: Mapped[dict] = mapped_column("org_assets_url", JSON, nullable=False, default={})
+    is_curated: Mapped[bool]  = mapped_column("is_curated", BOOLEAN, nullable=True, default=null)
+    description: Mapped[dict] = mapped_column("description", JSON, nullable=False, default={})
+    assets_hash: Mapped[dict] = mapped_column("assets_hash", JSON, nullable=False, default={})
+    contacts: Mapped[dict] = mapped_column("contacts", JSON, nullable=False, default={})
 
     created_on: Mapped[datetime] = mapped_column(
         "created_on", TIMESTAMP(timezone=False), nullable=False, server_default=func.now()
@@ -108,10 +108,10 @@ class Service(Base):
         index=True
     )
     service_id: Mapped[str] = mapped_column("service_id", VARCHAR(128), nullable=False)
-    service_path: Mapped[str] = mapped_column("service_path", VARCHAR(128), default=null)
-    hash_uri: Mapped[str] = mapped_column("hash_uri", VARCHAR(128), default=null)
-    is_curated: Mapped[bool] = mapped_column("is_curated", BOOLEAN, default=null)
-    service_email: Mapped[str] = mapped_column("service_email", VARCHAR(128), default=null)
+    service_path: Mapped[str] = mapped_column("service_path", VARCHAR(128), nullable=True, default=null)
+    hash_uri: Mapped[str] = mapped_column("hash_uri", VARCHAR(128), nullable=True, default=null)
+    is_curated: Mapped[bool] = mapped_column("is_curated", BOOLEAN, nullable=True, default=null)
+    service_email: Mapped[str] = mapped_column("service_email", VARCHAR(128), nullable=True, default=null)
 
     created_on: Mapped[datetime] = mapped_column(
         "created_on", TIMESTAMP(timezone=False), nullable=False, server_default=func.now()
@@ -143,8 +143,8 @@ class ServiceEndpoint(Base):
     org_id: Mapped[str] = mapped_column("org_id", VARCHAR(128), nullable=False)
     service_id: Mapped[str] = mapped_column("service_id", VARCHAR(128), nullable=False)
     group_id: Mapped[str] = mapped_column("group_id", VARCHAR(256), nullable=False)
-    endpoint: Mapped[str] = mapped_column("endpoint", VARCHAR(256), default=null)
-    is_available: Mapped[bool] = mapped_column("is_available", BOOLEAN, default=null)
+    endpoint: Mapped[str] = mapped_column("endpoint", VARCHAR(256), nullable=True, default=null)
+    is_available: Mapped[bool] = mapped_column("is_available", BOOLEAN, nullable=True, default=null)
     last_check_timestamp: Mapped[datetime] = mapped_column(
         "last_check_timestamp", TIMESTAMP(timezone=False), nullable=True, default=null
     )
@@ -177,12 +177,12 @@ class ServiceGroup(Base):
     org_id: Mapped[str] = mapped_column("org_id", VARCHAR(128), nullable=False)
     service_id: Mapped[str] = mapped_column("service_id", VARCHAR(128), nullable=False)
     free_call_signer_address: Mapped[str] = mapped_column(
-        "free_call_signer_address", VARCHAR(256), default=null
+        "free_call_signer_address", VARCHAR(256), nullable=True, default=null
     )
-    free_calls: Mapped[int] = mapped_column("free_calls", Integer, default=null)
+    free_calls: Mapped[int] = mapped_column("free_calls", Integer, nullable=True, default=null)
     group_id: Mapped[str] = mapped_column("group_id", VARCHAR(256), nullable=False)
     group_name: Mapped[str] = mapped_column("group_name", VARCHAR(128), nullable=False)
-    pricing: Mapped[dict] = mapped_column("pricing", JSON, default={})
+    pricing: Mapped[dict] = mapped_column("pricing", JSON, nullable=False, default={})
 
     created_on: Mapped[datetime] = mapped_column(
         "created_on", TIMESTAMP(timezone=False), nullable=False, server_default=func.now()
@@ -211,12 +211,12 @@ class ServiceMedia(Base):
     )
     org_id: Mapped[str] = mapped_column("org_id", VARCHAR(128), nullable=False)
     service_id: Mapped[str] = mapped_column("service_id", VARCHAR(128), nullable=False)
-    url: Mapped[str] = mapped_column("url", VARCHAR(512), default=null)
-    order: Mapped[int] = mapped_column("order", Integer, default=null)
-    file_type: Mapped[str] = mapped_column("file_type", VARCHAR(128), default=null)
-    asset_type: Mapped[str] = mapped_column("asset_type", VARCHAR(128), default=null)
-    alt_text: Mapped[str] = mapped_column("alt_text", VARCHAR(128), default=null)
-    hash_uri: Mapped[str] = mapped_column("hash_uri", VARCHAR(512), default=null)
+    url: Mapped[str] = mapped_column("url", VARCHAR(512), nullable=True, default=null)
+    order: Mapped[int] = mapped_column("order", Integer, nullable=True, default=null)
+    file_type: Mapped[str] = mapped_column("file_type", VARCHAR(128), nullable=True, default=null)
+    asset_type: Mapped[str] = mapped_column("asset_type", VARCHAR(128), nullable=True, default=null)
+    alt_text: Mapped[str] = mapped_column("alt_text", VARCHAR(128), nullable=True, default=null)
+    hash_uri: Mapped[str] = mapped_column("hash_uri", VARCHAR(512), nullable=True, default=null)
     
     created_on: Mapped[datetime] = mapped_column(
         "created_on", TIMESTAMP(timezone=False), nullable=False, server_default=func.now()
@@ -241,23 +241,23 @@ class ServiceMetadata(Base):
     )
     org_id: Mapped[str] = mapped_column("org_id", VARCHAR(128), nullable=False)
     service_id: Mapped[str] = mapped_column("service_id", VARCHAR(128), nullable=False)
-    display_name: Mapped[str] = mapped_column("display_name", VARCHAR(256), default=null)
-    description: Mapped[str] = mapped_column("description", VARCHAR(1024), default=null)
-    short_description: Mapped[str] = mapped_column("short_description", VARCHAR(1024), default=null)
+    display_name: Mapped[str] = mapped_column("display_name", VARCHAR(256), nullable=True, default=null)
+    description: Mapped[str] = mapped_column("description", VARCHAR(1024), nullable=True, default=null)
+    short_description: Mapped[str] = mapped_column("short_description", VARCHAR(1024), nullable=True, default=null)
     demo_component_available: Mapped[bool] = mapped_column(
         "demo_component_available", BOOLEAN, default=0, server_default="0", nullable=False
     )
-    url: Mapped[str] = mapped_column("url", VARCHAR(256), default=null)
-    json: Mapped[str] = mapped_column("json", VARCHAR(1024), default=null)
-    model_hash: Mapped[str] = mapped_column("model_hash", VARCHAR(256), default=null)
-    encoding: Mapped[str] = mapped_column("encoding", VARCHAR(128), default=null)
-    type: Mapped[str] = mapped_column("type", VARCHAR(128), default=null)
-    mpe_address: Mapped[str] = mapped_column("mpe_address", VARCHAR(256), default=null)
-    assets_url: Mapped[dict] = mapped_column("assets_url", JSON, default={})
-    assets_hash: Mapped[dict] = mapped_column("assets_hash", JSON, default={})
-    service_rating: Mapped[dict] = mapped_column("service_rating", JSON, default={})
+    url: Mapped[str] = mapped_column("url", VARCHAR(256), nullable=True, default=null)
+    json: Mapped[str] = mapped_column("json", VARCHAR(1024), nullable=True, default=null)
+    model_hash: Mapped[str] = mapped_column("model_hash", VARCHAR(256), nullable=True, default=null)
+    encoding: Mapped[str] = mapped_column("encoding", VARCHAR(128), nullable=True, default=null)
+    type: Mapped[str] = mapped_column("type", VARCHAR(128), nullable=True, default=null)
+    mpe_address: Mapped[str] = mapped_column("mpe_address", VARCHAR(256), nullable=True, default=null)
+    assets_url: Mapped[dict] = mapped_column("assets_url", JSON, nullable=False, default={})
+    assets_hash: Mapped[dict] = mapped_column("assets_hash", JSON, nullable=False, default={})
+    service_rating: Mapped[dict] = mapped_column("service_rating", JSON, nullable=False, default={})
     ranking: Mapped[int]  = mapped_column("ranking", Integer, default=1)
-    contributors: Mapped[dict] = mapped_column("contributors", JSON, default={})
+    contributors: Mapped[dict] = mapped_column("contributors", JSON, nullable=False, default={})
 
     created_on: Mapped[datetime] = mapped_column(
         "created_on", TIMESTAMP(timezone=False), nullable=False, server_default=func.now()
@@ -285,8 +285,8 @@ class ServiceTags(Base):
         index = True
     )
     org_id: Mapped[str] = mapped_column("org_id", VARCHAR(128), nullable=False)
-    service_id: Mapped[str] = mapped_column("service_id", VARCHAR(128), default=null)
-    tag_name: Mapped[str] = mapped_column("tag_name", VARCHAR(128), default=null)
+    service_id: Mapped[str] = mapped_column("service_id", VARCHAR(128), nullable=True, default=null)
+    tag_name: Mapped[str] = mapped_column("tag_name", VARCHAR(128), nullable=True, default=null)
     
     created_on: Mapped[datetime] = mapped_column(
         "created_on", TIMESTAMP(timezone=False), nullable=False, server_default=func.now()
