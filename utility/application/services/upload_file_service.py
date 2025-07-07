a1a3c4c7f2fc27f92aa217cf7a37d32338128959
elmiringos <elmir.iskhakov1337@gmail.com>
2025-07-07 15:53:30 +0300

    fix(utility): update store_file method to use org_uuid for ORG_ASSETS, add validation_handler in schemas, format code

--- utility/application/services/upload_file_service.py
+++ utility/application/services/upload_file_service.py
@@ -16,25 +16,21 @@ class UploadService:
         self.boto_utils = BotoUtils(region_name=settings.aws.REGION_NAME)
 
     def store_file(self, request: UploadFileRequest) -> dict[str, str]:
-
-        upload_type = request.type
-        content_type = request.content_type
-        raw_file_data = request.raw_file_data
-        file_extension = settings.files.FILE_EXTENSION[content_type]
+        file_extension = settings.files.FILE_EXTENSION[request.content_type]
         temp_file_path = os.path.join(TEMP_FILE_DIR, f"{uuid4().hex}_upload.{file_extension}")
         org_uuid = request.org_uuid
         service_uuid = request.service_uuid
 
-        with open(temp_file_path, 'wb') as file:
-            file.write(raw_file_data)
+        with open(temp_file_path, "wb") as file:
+            file.write(request.raw_file_data)
 
-        bucket = UPLOAD_TYPE_DETAILS[upload_type]["bucket"]
-        bucket_path = UPLOAD_TYPE_DETAILS[upload_type]["bucket_path"]
+        bucket = UPLOAD_TYPE_DETAILS[request.type]["bucket"]
+        bucket_path: str = UPLOAD_TYPE_DETAILS[request.type]["bucket_path"]
         bucket_path_params = [date_time_for_filename(), file_extension]
-        if upload_type == UploadType.FEEDBACK.value:
+        if request.type == UploadType.FEEDBACK.value:
             pass
-        elif upload_type == UploadType.ORG_ASSETS.value:
-            bucket_path_params = [service_uuid] + bucket_path_params
+        elif request.type == UploadType.ORG_ASSETS.value:
+            bucket_path_params = [org_uuid] + bucket_path_params
         else:
             bucket_path_params = [org_uuid, service_uuid] + bucket_path_params
 
