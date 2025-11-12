776353096f9216d055252e916c6c023b9d2ebd98
Arondondon <andreypravdin52@gmail.com>
2025-11-12 19:09:56 +0300

    feat(deployer): implement another logic of making daemon endpoint

--- deployer/utils.py
+++ deployer/utils.py
@@ -1,5 +1,10 @@
+import hashlib
+
 from deployer.config import HAAS_DAEMON_BASE_URL
 
 
 def get_daemon_endpoint(org_id: str, service_id: str) -> str:
-    return f"https://{org_id}-{service_id}.{HAAS_DAEMON_BASE_URL}"
+    org_service = f"{org_id}-{service_id}"
+    hash_org_service = hashlib.sha256(org_service.encode()).hexdigest()
+
+    return f"https://{hash_org_service}.{HAAS_DAEMON_BASE_URL}"
