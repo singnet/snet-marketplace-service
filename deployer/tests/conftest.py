7117ea97fa461746bdecd918721644dbd8f5fa46
Arondondon <andreypravdin52@gmail.com>
2026-01-30 18:32:16 +0300

    test(deployer): add db fixtures

---
+++ deployer/tests/conftest.py
@@ -0,0 +1,36 @@
+import time
+
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from testcontainers.mysql import MySqlContainer
+
+from deployer.infrastructure.models import Base
+
+
+@pytest.fixture(scope="session")
+def test_container():
+    with MySqlContainer("mysql:8", dialect="pymysql") as container:
+        yield container
+
+
+@pytest.fixture(scope = "session")
+def test_engine(test_container):
+    engine = create_engine(test_container.get_connection_url(), echo = False)
+
+    Base.metadata.drop_all(bind = engine)
+    Base.metadata.create_all(bind = engine)
+
+    yield engine
+
+    Base.metadata.drop_all(bind = engine)
+    engine.dispose()
+
+
+@pytest.fixture(scope = "function")
+def test_session_factory(test_engine):
+    return sessionmaker(test_engine, expire_on_commit = False)
+
+
+
+
